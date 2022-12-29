c9ed69c437e61cc6b613e1fee6d98580e1b4cc32
ilotterytea <ilotterytea@proton.me>
2022-12-29 23:29:43 +0600

    app constants

---
+++ src/main/java/kz/ilotterytea/bot/SharedConstants.java
@@ -0,0 +1,12 @@
+package kz.ilotterytea.bot;
+
+/**
+ * App constants.
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class SharedConstants {
+    public static final String PROPERTIES_PATH = "config.properties";
+
+    public static final String DEFAULT_PREFIX = "!";
+}
