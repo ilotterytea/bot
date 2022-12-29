0c95eaad0c9c1bd26dc1fb24a79b41cedcb7d19e
ilotterytea <ilotterytea@proton.me>
2022-12-30 05:22:44 +0600

    Ping command.

---
+++ src/main/java/kz/ilotterytea/bot/builtin/PingCommand.java
@@ -0,0 +1,36 @@
+package kz.ilotterytea.bot.builtin;
+
+import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
+import kz.ilotterytea.bot.Huinyabot;
+import kz.ilotterytea.bot.api.commands.Command;
+import kz.ilotterytea.bot.utils.StringUtils;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Ping command.
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class PingCommand extends Command {
+    @Override
+    public String getNameId() { return "ping"; }
+
+    @Override
+    public int getDelay() { return 5000; }
+
+    @Override
+    public String run(IRCMessageEvent ev) {
+        long uptime = ManagementFactory.getRuntimeMXBean().getUptime();
+
+        String ut = StringUtils.formatTimestamp(uptime / 1000);
+
+        return String.format(
+                "%s: DankCrouching \u2615 Java %s \u00b7 Uptime: %s \u00b7 TMI: %sms",
+                ev.getUser().getName(),
+                System.getProperty("java.version"),
+                ut,
+                Huinyabot.getClient().getChat().getLatency()
+        );
+    }
+}
