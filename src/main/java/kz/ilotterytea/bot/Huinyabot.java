4dad69a6907d031316fa9a21351885873a7c014c
ilotterytea <ilotterytea@proton.me>
2023-01-03 08:56:55 +0600

    Parsing channel IDs into channel names and then entering them into the chat.

--- src/main/java/kz/ilotterytea/bot/Huinyabot.java
+++ src/main/java/kz/ilotterytea/bot/Huinyabot.java
@@ -4,6 +4,7 @@ import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
+import com.github.twitch4j.helix.domain.User;
 import kz.ilotterytea.bot.api.commands.CommandLoader;
 import kz.ilotterytea.bot.api.delay.DelayManager;
 import kz.ilotterytea.bot.handlers.MessageHandlerSamples;
@@ -11,7 +12,10 @@ import kz.ilotterytea.bot.storage.PropLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * Bot.
@@ -38,20 +42,31 @@ public class Huinyabot extends Bot {
         delayer = new DelayManager();
 
         // - - -  T W I T C H  C L I E N T  - - - :
-        OAuth2Credential credential = new OAuth2Credential("twitch", properties.getProperty("OAUTH2_TOKEN", ""));
-        properties.setProperty("HB_BOT_USERNAME", (credential.getUserName() != null) ? credential.getUserName() : "");
+        OAuth2Credential credential = new OAuth2Credential("twitch", properties.getProperty("OAUTH2_TOKEN"));
 
         client = TwitchClientBuilder.builder()
                 .withChatAccount(credential)
                 .withEnableTMI(true)
                 .withEnableChat(true)
+                .withClientId(properties.getProperty("CLIENT_ID"))
+                .withEnableHelix(true)
                 .build();
 
         client.getChat().connect();
-        if (properties.getProperty("HB_BOT_USERNAME") != null) {
-            client.getChat().joinChannel(properties.getProperty("HB_BOT_USERNAME"));
+        if (credential.getUserName() != null) {
+            client.getChat().joinChannel(credential.getUserName());
         }
-        client.getChat().joinChannel("ilotterytea");
+        if (client.getHelix() != null && properties.getProperty("ACCESS_TOKEN") != null && properties.getProperty("INITIAL_CHANNEL_IDS") != null) {
+            List<User> userList = client.getHelix().getUsers(
+                    properties.getProperty("ACCESS_TOKEN"),
+                    Arrays.stream(properties.getProperty("INITIAL_CHANNEL_IDS").split(",")).collect(Collectors.toList()),
+                    null
+            ).execute().getUsers();
+
+            for (User u : userList) {
+                client.getChat().joinChannel(u.getLogin());
+            }
+        }
 
         client.getEventManager().onEvent(IRCMessageEvent.class, MessageHandlerSamples::ircMessageEvent);
     }
