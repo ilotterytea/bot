cab3c4b2aa4cfa901e2b42c1f310e32168e0b25b
ilotterytea <ilotterytea@proton.me>
2023-01-08 02:26:26 +0600

    7TV Websocket client.

--- src/main/java/kz/ilotterytea/bot/Huinyabot.java
+++ src/main/java/kz/ilotterytea/bot/Huinyabot.java
@@ -5,18 +5,22 @@ import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
 import com.github.twitch4j.helix.domain.User;
+import com.google.gson.Gson;
 import kz.ilotterytea.bot.api.commands.CommandLoader;
 import kz.ilotterytea.bot.api.delay.DelayManager;
 import kz.ilotterytea.bot.handlers.MessageHandlerSamples;
 import kz.ilotterytea.bot.storage.PropLoader;
 import kz.ilotterytea.bot.storage.json.TargetController;
 import kz.ilotterytea.bot.storage.json.UserController;
+import kz.ilotterytea.bot.thirdpartythings.seventv.v1.SevenTVWebsocketClient;
+import kz.ilotterytea.bot.thirdpartythings.seventv.v1.models.Message;
 import kz.ilotterytea.bot.utils.StorageUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 /**
@@ -31,6 +35,7 @@ public class Huinyabot extends Bot {
     private DelayManager delayer;
     private TargetController targets;
     private UserController users;
+    private SevenTVWebsocketClient sevenTV;
 
     private final Logger LOGGER = LoggerFactory.getLogger(Huinyabot.class);
 
@@ -40,6 +45,7 @@ public class Huinyabot extends Bot {
     public DelayManager getDelayer() { return delayer; }
     public TargetController getTargetCtrl() { return targets; }
     public UserController getUserCtrl() { return users; }
+    public SevenTVWebsocketClient getSevenTVWSClient() { return sevenTV; }
 
     private static Huinyabot instance;
     public static Huinyabot getInstance() { return instance; }
@@ -54,6 +60,13 @@ public class Huinyabot extends Bot {
         properties = new PropLoader(SharedConstants.PROPERTIES_PATH);
         loader = new CommandLoader();
         delayer = new DelayManager();
+
+        try {
+            sevenTV = new SevenTVWebsocketClient();
+            sevenTV.connectBlocking();
+        } catch (URISyntaxException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         // - - -  T W I T C H  C L I E N T  - - - :
         OAuth2Credential credential = new OAuth2Credential("twitch", properties.getProperty("OAUTH2_TOKEN"));
@@ -70,8 +83,10 @@ public class Huinyabot extends Bot {
         if (credential.getUserName() != null) {
             client.getChat().joinChannel(credential.getUserName());
         }
+
+        List<User> userList = new ArrayList<>();
         if (client.getHelix() != null && properties.getProperty("ACCESS_TOKEN") != null && targets.getAll().keySet().size() > 0) {
-            List<User> userList = client.getHelix().getUsers(
+            userList = client.getHelix().getUsers(
                     properties.getProperty("ACCESS_TOKEN"),
                     new ArrayList<>(targets.getAll().keySet()),
                     null
@@ -82,12 +97,17 @@ public class Huinyabot extends Bot {
             }
         }
 
+        for (User user : userList) {
+            sevenTV.send(new Gson().toJson(new Message("join", user.getLogin())));
+        }
+
         client.getEventManager().onEvent(IRCMessageEvent.class, MessageHandlerSamples::ircMessageEvent);
     }
 
     @Override
     public void dispose() {
         client.close();
+        sevenTV.close();
         targets.save();
         users.save();
     }
