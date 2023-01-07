08a400861dc88450d3eb7d3bb1b99055bba53804
ilotterytea <ilotterytea@proton.me>
2023-01-07 20:09:18 +0600

    Spam command.

---
+++ src/main/java/kz/ilotterytea/bot/builtin/SpamCommand.java
@@ -0,0 +1,69 @@
+package kz.ilotterytea.bot.builtin;
+
+import kz.ilotterytea.bot.Huinyabot;
+import kz.ilotterytea.bot.api.commands.Command;
+import kz.ilotterytea.bot.api.permissions.Permissions;
+import kz.ilotterytea.bot.models.ArgumentsModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Spam command.
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class SpamCommand extends Command {
+    @Override
+    public String getNameId() { return "spam"; }
+
+    @Override
+    public int getDelay() { return 30000; }
+
+    @Override
+    public Permissions getPermissions() { return Permissions.MOD; }
+
+    @Override
+    public ArrayList<String> getOptions() { return new ArrayList<>(List.of(new String[]{"count"})); }
+
+    @Override
+    public ArrayList<String> getSubcommands() { return new ArrayList<>(); }
+
+    @Override
+    public ArrayList<String> getAliases() { return new ArrayList<>(List.of(new String[]{"спам", "насрать", "repeat", "cv", "paste", "cvpaste"})); }
+
+    @Override
+    public String run(ArgumentsModel m) {
+        final int MAX_COUNT = 8;
+        ArrayList<String> s = new ArrayList<>(List.of(m.getMessage().getMessage().split(" ")));
+
+        if (s.size() <= 1) {
+            return "No message found.";
+        }
+        int count;
+
+        try {
+            count = Integer.parseInt(s.get(0));
+            s.remove(0);
+        } catch (NumberFormatException e) {
+            return "No spam count found.";
+        }
+
+        if (count > MAX_COUNT) {
+            count = MAX_COUNT;
+        }
+
+        for (int i = 0; i < count; i++) {
+            Huinyabot.getInstance().getClient().getChat().sendMessage(
+                    m.getEvent().getChannel().getName(),
+                    String.format(
+                            "%s %s",
+                            String.join(" ", s),
+                            (m.getMessage().getOptions().contains("count")) ? i + 1 : ""
+                    )
+            );
+        }
+
+        return null;
+    }
+}
