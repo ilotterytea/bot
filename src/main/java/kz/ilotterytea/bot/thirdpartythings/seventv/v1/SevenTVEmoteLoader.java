d649e42c1c5d037423d8101b656350b1c3d8fa99
ilotterytea <ilotterytea@proton.me>
2023-01-08 15:16:26 +0600

    The 7TV emote loader.

---
+++ src/main/java/kz/ilotterytea/bot/thirdpartythings/seventv/v1/SevenTVEmoteLoader.java
@@ -0,0 +1,57 @@
+package kz.ilotterytea.bot.thirdpartythings.seventv.v1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import kz.ilotterytea.bot.SharedConstants;
+import kz.ilotterytea.bot.net.HttpFactory;
+import kz.ilotterytea.bot.net.models.Response;
+import kz.ilotterytea.bot.thirdpartythings.EmoteLoader;
+import kz.ilotterytea.bot.thirdpartythings.seventv.v1.models.EmoteAPIData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+/**
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class SevenTVEmoteLoader implements EmoteLoader<EmoteAPIData> {
+    private final Logger LOGGER = LoggerFactory.getLogger(SevenTVEmoteLoader.class);
+
+    @Override
+    public ArrayList<EmoteAPIData> getChannelEmotes(String channelName) {
+        Response response = HttpFactory.sendGETRequest(String.format(SharedConstants.STV_CHANNEL_EMOTES_URL, channelName));
+
+        if (response == null || response.getResponse() == null || response.getCode() != 200) {
+            LOGGER.debug("No response.");
+            return null;
+        }
+
+        ArrayList<EmoteAPIData> data = new Gson().fromJson(response.getResponse(), new TypeToken<ArrayList<EmoteAPIData>>(){}.getType());
+
+        for (EmoteAPIData emote : data) {
+            System.out.println(emote.getName());
+        }
+
+        return data;
+    }
+
+    @Override
+    public ArrayList<EmoteAPIData> getGlobalEmotes() {
+        Response response = HttpFactory.sendGETRequest(SharedConstants.STV_GLOBAL_EMOTES_URL);
+
+        if (response == null || response.getResponse() == null || response.getCode() != 200) {
+            LOGGER.debug("No response.");
+            return null;
+        }
+
+        ArrayList<EmoteAPIData> data = new Gson().fromJson(response.getResponse(), new TypeToken<ArrayList<EmoteAPIData>>(){}.getType());
+
+        for (EmoteAPIData emote : data) {
+            System.out.println(emote.getName());
+        }
+
+        return data;
+    }
+}
