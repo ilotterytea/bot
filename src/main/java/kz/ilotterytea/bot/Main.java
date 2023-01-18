4c36a3c62f0dd9be97042d1ab4f38bcf25f5cf56
ilotterytea <ilotterytea@proton.me>
2023-01-19 00:45:04 +0600

    Enable micronaut

--- src/main/java/kz/ilotterytea/bot/Main.java
+++ src/main/java/kz/ilotterytea/bot/Main.java
@@ -1,11 +1,17 @@
 package kz.ilotterytea.bot;
 
+import io.micronaut.runtime.Micronaut;
+
 /**
  * @author ilotterytea
  * @since 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Micronaut.build(args)
+                .classes(Main.class)
+                .start();
+
         Huinyabot bot = new Huinyabot();
 
         Runtime.getRuntime().addShutdownHook(new Thread(bot::dispose));
