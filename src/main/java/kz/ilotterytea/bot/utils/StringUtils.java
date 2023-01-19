9f3984bba5a556f1a955ea5703179c873aafcda1
ilotterytea <ilotterytea@proton.me>
2023-01-19 18:28:17 +0600

    Number formatter

--- src/main/java/kz/ilotterytea/bot/utils/StringUtils.java
+++ src/main/java/kz/ilotterytea/bot/utils/StringUtils.java
@@ -1,6 +1,8 @@
 package kz.ilotterytea.bot.utils;
 
-import java.lang.management.ManagementFactory;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 /**
  * String utilities.
@@ -42,5 +44,30 @@ public class StringUtils {
         else {
             return String.format("%sd%sh", d, h);
         }
+    }
+
+    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    static {
+        suffixes.put(1_000L, "k");
+        suffixes.put(1_000_000L, "M");
+        suffixes.put(1_000_000_000L, "G");
+        suffixes.put(1_000_000_000_000L, "T");
+        suffixes.put(1_000_000_000_000_000L, "P");
+        suffixes.put(1_000_000_000_000_000_000L, "E");
+    }
+
+    public static String formatNumber(long value) {
+        //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
+        if (value == Long.MIN_VALUE) return formatNumber(Long.MIN_VALUE + 1);
+        if (value < 0) return "-" + formatNumber(-value);
+        if (value < 1000) return Long.toString(value); //deal with easy case
+
+        Map.Entry<Long, String> e = suffixes.floorEntry(value);
+        Long divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
     }
 }
