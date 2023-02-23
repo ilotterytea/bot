e57ad15e9744b38c03990e2f26ba218013e3a3a0
ilotterytea <ilotterytea@proton.me>
2023-02-23 22:29:37 +0600

    Renamed arguments to services

--- src/services.rs
+++ src/services.rs
@@ -1,11 +1,8 @@
-use twitch_irc::{
-    login::StaticLoginCredentials, message::PrivmsgMessage, SecureTCPTransport, TwitchIRCClient,
-};
+use twitch_irc::{login::StaticLoginCredentials, SecureTCPTransport, TwitchIRCClient};
 
 use crate::managers::command_loader::CommandLoader;
 
-pub struct Arguments {
-    pub message: PrivmsgMessage,
+pub struct Services {
     pub client: &'static TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>,
     pub loader: &'static CommandLoader,
 }
