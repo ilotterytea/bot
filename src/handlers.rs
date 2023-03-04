17326ba120e4d5121260f512a5fd9c4da46dc073
ilotterytea <ilotterytea@proton.me>
2023-03-04 23:49:52 +0600

    added more checks

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,7 +2,10 @@ use twitch_irc::{
     login::StaticLoginCredentials, message::PrivmsgMessage, SecureTCPTransport, TwitchIRCClient,
 };
 
-use crate::{establish_connection, managers::command_loader::CommandLoader};
+use crate::{
+    arguments::Arguments, commands::MessageCommandArguments, establish_connection,
+    managers::command_loader::CommandLoader,
+};
 
 pub async fn irc_message_handler(
     client: &TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>,
@@ -16,20 +19,36 @@ pub async fn irc_message_handler(
             .expect("Unable to send a message to chat!");
     }
 
-    let response = loader.run(
+    let args = Arguments::generate(
         &mut establish_connection(),
-        &message.message_text.as_str(),
-        &message.sender.id.as_str(),
-        &message.channel_id.as_str(),
+        message.sender.id.clone(),
+        message.channel_id.clone(),
     );
 
-    if !&response.is_none() {
-        client
-            .say(
-                message.channel_login.to_owned(),
-                response.unwrap().to_owned(),
-            )
-            .await
-            .expect("Unable to send a message to chat!");
+    let prefix = args.preferences.prefix.clone().unwrap_or("!".to_string());
+
+    if message.message_text.starts_with(prefix.as_str()) {
+        let cmd_args =
+            MessageCommandArguments::parse(loader, &message.message_text.as_str(), prefix.as_str());
+
+        if cmd_args.is_some() {
+            let response = loader.run(
+                &mut establish_connection(),
+                cmd_args.unwrap(),
+                args,
+                &message.sender.id.as_str(),
+                &message.channel_id.as_str(),
+            );
+
+            if !&response.is_none() {
+                client
+                    .say(
+                        message.channel_login.to_owned(),
+                        response.unwrap().to_owned(),
+                    )
+                    .await
+                    .expect("Unable to send a message to chat!");
+            }
+        }
     }
 }
