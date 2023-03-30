174bfff61f85ae2589022e07a0874641826c7ff9
ilotterytea <ilotterytea@proton.me>
2023-03-30 22:53:14 +0600

    Channel entity

---
+++ src/main/java/kz/ilotterytea/bot/entities/channels/Channel.java
@@ -0,0 +1,39 @@
+package kz.ilotterytea.bot.entities.channels;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "channels")
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(name = "alias_id", updatable = false, nullable = false, unique = true)
+    private Integer aliasId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    public Channel(Integer aliasId) {
+        this.aliasId = aliasId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getAliasId() {
+        return aliasId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
