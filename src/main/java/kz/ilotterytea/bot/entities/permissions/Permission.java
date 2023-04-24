098501dab59cbd87ee7d8fe97d26d4b446ba5f2e
ilotterytea <ilotterytea@proton.me>
2023-04-25 00:02:59 +0600

    add: user permissions entity

---
+++ src/main/java/kz/ilotterytea/bot/entities/permissions/Permission.java
@@ -0,0 +1,14 @@
+package kz.ilotterytea.bot.entities.permissions;
+
+/**
+ * @author ilotterytea
+ * @version 1.4
+ */
+public enum Permission {
+    SUSPENDED,
+    USER,
+    VIP,
+    MOD,
+    BROADCASTER,
+    SUPERUSER
+}
---
+++ src/main/java/kz/ilotterytea/bot/entities/permissions/UserPermission.java
@@ -0,0 +1,61 @@
+package kz.ilotterytea.bot.entities.permissions;
+
+import jakarta.persistence.*;
+import kz.ilotterytea.bot.entities.channels.Channel;
+import kz.ilotterytea.bot.entities.users.User;
+
+/**
+ * @author ilotterytea
+ * @version 1.4
+ */
+public class UserPermission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, updatable = false, nullable = false)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "channel_id", updatable = false, nullable = false)
+    private Channel channel;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", updatable = false, nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Permission permission;
+
+    public UserPermission() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+}
