2bfbf8fd2780c3be80c3713873173bdb8bd23322
ilotterytea <ilotterytea@proton.me>
2023-05-25 15:04:03 +0600

    add: constructors

--- src/main/java/kz/ilotterytea/bot/entities/events/EventFlag.java
+++ src/main/java/kz/ilotterytea/bot/entities/events/EventFlag.java
@@ -1,5 +1,7 @@
 package kz.ilotterytea.bot.entities.events;
 
+import java.util.Optional;
+
 /**
  * Flags for events.
  * @author ilotterytea
@@ -10,11 +12,31 @@ public enum EventFlag {
      * A flag for massping.
      * With this flag, a list of chatters will be added to the list of usernames.
      */
-    MASSPING,
+    MASSPING("massping"),
 
     /**
      * Non-subscription flag.
      * Forbids subscribing to an event with this flag.
      */
-    NON_SUBSCRIPTION
+    NON_SUBSCRIPTION("non_subscription");
+
+    private final String name;
+
+    EventFlag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Optional<EventFlag> findEventFlagById(String id) {
+        for (EventFlag eventFlag : EventFlag.values()) {
+            if (eventFlag.name.equals(id)) {
+                return Optional.of(eventFlag);
+            }
+        }
+
+        return Optional.empty();
+    }
 }
--- src/main/java/kz/ilotterytea/bot/entities/events/EventType.java
+++ src/main/java/kz/ilotterytea/bot/entities/events/EventType.java
@@ -1,5 +1,7 @@
 package kz.ilotterytea.bot.entities.events;
 
+import java.util.Optional;
+
 /**
  * Types for event.
  * @author ilotterytea
@@ -9,15 +11,35 @@ public enum EventType {
     /**
      * Custom event type.
      */
-    CUSTOM,
+    CUSTOM("custom"),
 
     /**
      * "Stream live" event type.
      */
-    LIVE,
+    LIVE("live"),
 
     /**
      * "Stream offline" event type.
      */
-    OFFLINE
+    OFFLINE("offline");
+
+    private final String name;
+
+    EventType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Optional<EventType> findEventTypeById(String id) {
+        for (EventType eventType : EventType.values()) {
+            if (eventType.name.equals(id)) {
+                return Optional.of(eventType);
+            }
+        }
+
+        return Optional.empty();
+    }
 }
