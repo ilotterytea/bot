456928852ea3806e86da1cfe1be265a81c8492d4
ilotterytea <ilotterytea@proton.me>
2023-05-28 00:14:40 +0600

    fix: im a zevaka

--- src/main/java/kz/ilotterytea/bot/handlers/StreamEventHandlers.java
+++ src/main/java/kz/ilotterytea/bot/handlers/StreamEventHandlers.java
@@ -56,7 +56,7 @@ public class StreamEventHandlers {
      */
     private static List<Event> getEventsByAliasId(String aliasId, EventType eventType) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        List<Event> events = session.createQuery("from Events where aliasId = :aliasId AND eventType = :eventType", Event.class)
+        List<Event> events = session.createQuery("from Event where aliasId = :aliasId AND eventType = :eventType", Event.class)
                 .setParameter("aliasId", aliasId)
                 .setParameter("eventType", eventType)
                 .getResultList();
