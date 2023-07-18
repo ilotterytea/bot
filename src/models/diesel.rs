11c65980043baa994d444f5104db5890b55f3162
ilotterytea <ilotterytea@proton.me>
2023-07-18 19:33:19 +0600

    add: event subscription tables + diesel-derive-enum dependency

--- src/models/diesel.rs
+++ src/models/diesel.rs
@@ -2,6 +2,22 @@ use crate::schema::*;
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
 
+#[derive(diesel_derive_enum::DbEnum, Debug, PartialEq)]
+#[ExistingTypePath = "crate::schema::sql_types::EventType"]
+pub enum EventType {
+    Live,
+    Offline,
+    Title,
+    Game,
+    Custom,
+}
+
+#[derive(diesel_derive_enum::DbEnum, Debug, PartialEq)]
+#[ExistingTypePath = "crate::schema::sql_types::EventFlag"]
+pub enum EventFlag {
+    Massping,
+}
+
 #[derive(Queryable)]
 pub struct Channel {
     pub id: i32,
@@ -32,4 +48,42 @@ pub struct User {
 pub struct NewUser<'a> {
     pub alias_id: i32,
     pub alias_name: &'a str,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(Channel))]
+pub struct Event {
+    pub id: i32,
+    pub channel_id: i32,
+    pub target_alias_id: Option<i32>,
+    pub custom_alias_id: Option<String>,
+    pub message: String,
+    pub event_type: EventType,
+    pub flags: Vec<EventFlag>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = events)]
+pub struct NewEvent<'a> {
+    pub channel_id: i32,
+    pub target_alias_id: Option<i32>,
+    pub custom_alias_id: Option<&'a str>,
+    pub message: &'a str,
+    pub event_type: EventType,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(Event))]
+#[diesel(belongs_to(User))]
+pub struct EventSubscription {
+    pub id: i32,
+    pub event_id: i32,
+    pub user_id: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = event_subscriptions)]
+pub struct NewEventSubscription {
+    pub event_id: i32,
+    pub user_id: i32,
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,5 +1,15 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "event_flag"))]
+    pub struct EventFlag;
+
+    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "event_type"))]
+    pub struct EventType;
+}
+
 diesel::table! {
     channels (id) {
         id -> Int4,
@@ -8,6 +18,32 @@ diesel::table! {
         alias_name -> Varchar,
         joined_at -> Timestamp,
         opt_outed_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    event_subscriptions (id) {
+        id -> Int4,
+        event_id -> Int4,
+        user_id -> Int4,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::EventType;
+    use super::sql_types::EventFlag;
+
+    events (id) {
+        id -> Int4,
+        channel_id -> Int4,
+        target_alias_id -> Nullable<Int4>,
+        #[max_length = 500]
+        custom_alias_id -> Nullable<Varchar>,
+        #[max_length = 500]
+        message -> Varchar,
+        event_type -> EventType,
+        flags -> Array<Nullable<EventFlag>>,
     }
 }
 
@@ -22,7 +58,13 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(event_subscriptions -> events (event_id));
+diesel::joinable!(event_subscriptions -> users (user_id));
+diesel::joinable!(events -> channels (channel_id));
+
 diesel::allow_tables_to_appear_in_same_query!(
     channels,
+    event_subscriptions,
+    events,
     users,
 );
