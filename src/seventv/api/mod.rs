b241b024278e6d61d9b9e90ba296f88a9174c4f4
ilotterytea <ilotterytea@proton.me>
2023-08-19 14:40:02 +0600

    add: 7tv api client + get user by twitch id function

--- src/seventv/api/mod.rs
+++ src/seventv/api/mod.rs
@@ -1,1 +1,30 @@
+use reqwest::Client;
+
+use self::schemes::ConnectionUser;
+
 pub mod schemes;
+
+pub struct SevenTVAPIClient {
+    client: Client,
+}
+
+const SEVENTV_URL: &str = "https://7tv.io/v3";
+
+impl SevenTVAPIClient {
+    pub fn new(client: Client) -> Self {
+        Self { client }
+    }
+
+    pub async fn get_user_by_twitch_id(&self, user_id: String) -> Option<ConnectionUser> {
+        let url = format!("{SEVENTV_URL}/users/twitch/{user_id}");
+        let request = self.client.get(url).send().await;
+
+        if let Ok(response) = request {
+            if let Ok(data) = response.json::<ConnectionUser>().await {
+                return Some(data);
+            }
+        }
+
+        None
+    }
+}
