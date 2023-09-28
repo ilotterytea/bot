9994d632d9e8a9c5ead2d80404ef96089ba27d7c
ilotterytea <me@ilotterytea.kz>
2023-09-28 10:39:20 +0600

    feat: twitch irc dependencies + example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use twitch_irc::{
+    login::StaticLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
+    TwitchIRCClient,
+};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let (mut irc_incoming_messages, irc_client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(ClientConfig::default());
+
+    irc_client.join("ilotterytea".into()).unwrap();
+
+    let irc_thread = tokio::spawn(async move {
+        while let Some(irc_message) = irc_incoming_messages.recv().await {
+            match irc_message {
+                ServerMessage::Privmsg(message) => {
+                    println!("received message: {:?}", message);
+                }
+                _ => {
+                    println!("not handled message: {:?}", irc_message);
+                }
+            }
+        }
+    });
+
+    tokio::join!(irc_thread);
 }
