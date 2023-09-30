38a6f26529d5969a7a6a7d66899a36eed251bc6c
ilotterytea <me@ilotterytea.kz>
2023-10-01 00:58:10 +0600

    feat: localizator

---
+++ src/localization.rs
@@ -0,0 +1,46 @@
+use include_dir::{include_dir, Dir};
+use std::{collections::HashMap, str::from_utf8};
+
+#[derive(PartialEq, Eq, Hash)]
+pub enum LineId {
+    CommandPingResponse,
+}
+
+impl LineId {
+    pub fn from_string(value: String) -> Option<Self> {
+        match value.as_str() {
+            "cmd.ping.response" => Some(Self::CommandPingResponse),
+            _ => None,
+        }
+    }
+}
+
+pub struct Localizator {
+    localizations: HashMap<String, HashMap<LineId, String>>,
+}
+
+const LOCALIZATION_DIR: Dir = include_dir!("localizations");
+
+impl Localizator {
+    pub fn new() -> Self {
+        let mut localizations: HashMap<String, HashMap<LineId, String>> = HashMap::new();
+
+        for file in LOCALIZATION_DIR.files() {
+            let file_name = file.path();
+            let language = file_name.file_stem().and_then(|s| s.to_str()).unwrap();
+
+            let contents = from_utf8(file.contents()).expect("Failed to read file");
+            let data: HashMap<String, String> = serde_json::from_str(contents).unwrap();
+
+            let map = localizations.entry(language.to_string()).or_default();
+
+            for (line_id, line) in data {
+                if let Some(line_id) = LineId::from_string(line_id) {
+                    map.insert(line_id, line);
+                }
+            }
+        }
+
+        Self { localizations }
+    }
+}
