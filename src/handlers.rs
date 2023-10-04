8b5754275ef155bdc895708cbecb4c53aa7a45af
ilotterytea <me@ilotterytea.kz>
2023-10-04 11:35:00 +0600

    feat: getting channel and sender data from the database

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,13 @@
+use diesel::{insert_into, BelongingToDsl, ExpressionMethods, QueryDsl, RunQueryDsl};
 use twitch_irc::message::PrivmsgMessage;
 
 use crate::{
-    command::CommandLoader, instance_bundle::InstanceBundle, message::ParsedPrivmsgMessage,
+    command::CommandLoader,
+    instance_bundle::InstanceBundle,
+    message::ParsedPrivmsgMessage,
+    models::diesel::{Channel, ChannelPreference, NewChannel, NewChannelPreference, NewUser, User},
+    schema::{channel_preferences::dsl as chp, channels::dsl as ch, users::dsl as us},
+    utils::establish_connection,
 };
 
 pub async fn handle_chat_message(
@@ -10,6 +16,56 @@ pub async fn handle_chat_message(
     message: PrivmsgMessage,
 ) {
     let parsed_message = ParsedPrivmsgMessage::parse(message.message_text.as_str(), '~');
+    let conn = &mut establish_connection();
+
+    let channel = ch::channels
+        .filter(ch::alias_id.eq(message.channel_id.parse::<i32>().unwrap()))
+        .first::<Channel>(conn)
+        .unwrap_or_else(|_| {
+            insert_into(ch::channels)
+                .values(vec![NewChannel {
+                    alias_id: message.channel_id.parse::<i32>().unwrap(),
+                }])
+                .execute(conn)
+                .expect("Failed to create a new channel");
+
+            ch::channels
+                .filter(ch::alias_id.eq(message.channel_id.parse::<i32>().unwrap()))
+                .first::<Channel>(conn)
+                .expect("Failed to get a channel after creating it")
+        });
+
+    let channel_preference = ChannelPreference::belonging_to(&channel)
+        .first::<ChannelPreference>(conn)
+        .unwrap_or_else(|_| {
+            insert_into(chp::channel_preferences)
+                .values(vec![NewChannelPreference {
+                    channel_id: channel.id,
+                }])
+                .execute(conn)
+                .expect("Failed to create preferences for channel");
+
+            ChannelPreference::belonging_to(&channel)
+                .first::<ChannelPreference>(conn)
+                .expect("Failed to get preferences after creating them")
+        });
+
+    let user = us::users
+        .filter(us::alias_id.eq(message.sender.id.parse::<i32>().unwrap()))
+        .first::<User>(conn)
+        .unwrap_or_else(|_| {
+            insert_into(us::users)
+                .values(vec![NewUser {
+                    alias_id: message.sender.id.parse::<i32>().unwrap(),
+                }])
+                .execute(conn)
+                .expect("Failed to create a new user");
+
+            us::users
+                .filter(us::alias_id.eq(message.sender.id.parse::<i32>().unwrap()))
+                .first::<User>(conn)
+                .expect("Failed to get a user after creating it")
+        });
 
     if let Some(parsed_message) = parsed_message {
         if let Ok(Some(response)) = command_loader
