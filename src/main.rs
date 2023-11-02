2e363170b06cca5d433b2b30f71e0cbeebc4d972
ilotterytea <me@ilotterytea.kz>
2023-11-02 12:14:48 +0600

    feat: join chats from database

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::{env, sync::Arc};
 
 use crate::{
     command::CommandLoader, handlers::handle_chat_message, instance_bundle::InstanceBundle,
-    localization::Localizator, shared_variables::START_TIME,
+    localization::Localizator, schema::channels::dsl as ch, shared_variables::START_TIME,
+    utils::establish_connection,
 };
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
 use eyre::Context;
 use reqwest::Client;
 use twitch_api::{
     client::ClientDefault,
     twitch_oauth2::{AccessToken, UserToken},
+    types::UserIdRef,
     HelixClient,
 };
 use twitch_irc::{
@@ -76,7 +79,33 @@ async fn main() {
 
     let helix_client = Arc::new(HelixClient::with_client(reqwest_client));
 
-    irc_client.join("imteabot".into()).unwrap();
+    let conn = &mut establish_connection();
+
+    let channel_alias_ids = ch::channels
+        .filter(ch::opt_outed_at.is_null())
+        .select(ch::alias_id)
+        .load::<i32>(conn)
+        .expect("Failed to get alias IDs");
+
+    for id in channel_alias_ids {
+        let id = id.to_string();
+
+        if let Ok(Some(user)) = helix_client
+            .get_user_from_id(UserIdRef::from_str(id.as_str()), &*helix_token)
+            .await
+        {
+            irc_client
+                .join(user.login.to_string())
+                .expect("Failed to join a channel");
+
+            println!("Successfully joined #{}", &user.login);
+        } else {
+            println!(
+                "Failed to get user data with ID {} when joining channels",
+                id
+            );
+        }
+    }
 
     let irc_thread = tokio::spawn(async move {
         while let Some(irc_message) = irc_incoming_messages.recv().await {
