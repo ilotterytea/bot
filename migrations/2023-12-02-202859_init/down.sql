9d4e9b8773463ae8ce64e29409bce7f4e813ace8
ilotterytea <me@ilotterytea.kz>
2023-12-03 17:58:51 +0600

    change: recreated diesel models

--- migrations/2023-12-02-202859_init/down.sql
+++ migrations/2023-12-02-202859_init/down.sql
@@ -1,4 +1,6 @@
 -- This file should undo anything in `up.sql`
+DROP TABLE "actions";
 DROP TABLE "channel_preferences";
 DROP TABLE "channels";
 DROP TABLE "users";
+DROP TYPE "action_statuses";
--- src/models/diesel.rs
+++ src/models/diesel.rs
@@ -2,10 +2,11 @@ use crate::schema::*;
 use chrono::NaiveDateTime;
 use diesel::{Associations, Identifiable, Insertable, Queryable};
 
-#[derive(Queryable, Identifiable)]
+#[derive(Queryable, Identifiable, Clone)]
 pub struct Channel {
     pub id: i32,
     pub alias_id: i32,
+    pub alias_name: String,
     pub joined_at: NaiveDateTime,
     pub opt_outed_at: Option<NaiveDateTime>,
 }
@@ -14,37 +15,48 @@ pub struct Channel {
 #[diesel(table_name = channels)]
 pub struct NewChannel {
     pub alias_id: i32,
+    pub alias_name: String,
 }
 
-#[derive(Queryable, Identifiable, Associations)]
+#[derive(Queryable, Identifiable, Associations, Clone)]
 #[diesel(belongs_to(Channel, foreign_key = channel_id))]
 #[diesel(table_name = channel_preferences)]
 pub struct ChannelPreference {
     pub id: i32,
     pub channel_id: i32,
-    pub prefix: Option<String>,
-    pub language: Option<String>,
+    pub prefix: String,
+    pub language: String,
 }
 
 #[derive(Insertable)]
 #[diesel(table_name = channel_preferences)]
 pub struct NewChannelPreference {
     pub channel_id: i32,
+    pub prefix: String,
+    pub language: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Clone)]
 pub struct User {
     pub id: i32,
     pub alias_id: i32,
+    pub alias_name: String,
     pub joined_at: NaiveDateTime,
     pub opt_outed_at: Option<NaiveDateTime>,
-    pub is_super_user: bool,
 }
 
 #[derive(Insertable)]
 #[diesel(table_name = users)]
 pub struct NewUser {
     pub alias_id: i32,
+    pub alias_name: String,
+}
+
+#[derive(diesel_derive_enum::DbEnum, Debug, PartialEq)]
+#[ExistingTypePath = "crate::schema::sql_types::ActionStatuses"]
+pub enum ActionStatus {
+    Ok,
+    Error,
 }
 
 #[derive(Queryable, Identifiable, Associations)]
@@ -55,11 +67,12 @@ pub struct Action {
     pub id: i32,
     pub channel_id: i32,
     pub user_id: i32,
-    pub command: String,
-    pub attributes: Option<String>,
-    pub full_message: String,
-    pub response: Option<String>,
-    pub timestamp: NaiveDateTime,
+    pub command_name: String,
+    pub arguments: Option<String>,
+    pub response: String,
+    pub status: ActionStatus,
+    pub sent_at: NaiveDateTime,
+    pub processed_at: NaiveDateTime,
 }
 
 #[derive(Insertable)]
@@ -67,8 +80,10 @@ pub struct Action {
 pub struct NewAction {
     pub channel_id: i32,
     pub user_id: i32,
-    pub command: String,
-    pub attributes: Option<String>,
-    pub full_message: String,
-    pub response: Option<String>,
+    pub command_name: String,
+    pub arguments: Option<String>,
+    pub response: String,
+    pub status: ActionStatus,
+    pub sent_at: NaiveDateTime,
+    pub processed_at: NaiveDateTime,
 }
