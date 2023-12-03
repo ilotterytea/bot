02b5c483d1e805d12194d965aae209b10c159468
ilotterytea <me@ilotterytea.kz>
2023-12-03 18:03:24 +0600

    change: DEFAULT_PREFIX type from char to &str

--- src/shared_variables.rs
+++ src/shared_variables.rs
@@ -9,7 +9,7 @@ lazy_static! {
 pub const DEFAULT_COMMAND_DELAY_SEC: i32 = 5;
 pub const DEFAULT_COMMAND_OPTIONS: Vec<String> = Vec::new();
 
-pub const DEFAULT_PREFIX: char = '~';
+pub const DEFAULT_PREFIX: &str = "~";
 pub const DEFAULT_LANGUAGE: &str = "english";
 
 pub const HOLIDAY_V1_API_URL: &str = "https://hol.ilotterytea.kz/api/v1";
