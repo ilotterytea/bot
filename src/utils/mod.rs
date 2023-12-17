1176fac96ead8d75c5350655541d093e944ba19d
ilotterytea <me@ilotterytea.kz>
2023-12-17 21:43:09 +0600

    feat: method for splitting and wrapping lines

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -19,3 +19,28 @@ pub fn format_timestamp(timestamp_in_seconds: u64) -> String {
         format!("{}d{}h", days, hours)
     }
 }
+
+pub fn split_and_wrap_lines(
+    init_string: &str,
+    separator: &str,
+    max_length_per_line: usize,
+) -> Vec<String> {
+    let input_lines = init_string.split(separator).collect::<Vec<&str>>();
+    let mut output_lines: Vec<String> = Vec::new();
+    let mut buffer_lines: Vec<String> = Vec::new();
+
+    for line in input_lines {
+        let buffer_string = buffer_lines.join(separator);
+
+        if buffer_string.len() + line.len() + separator.len() >= max_length_per_line {
+            output_lines.push(buffer_string);
+            buffer_lines.clear();
+        }
+
+        buffer_lines.push(line.to_string());
+    }
+
+    output_lines.push(buffer_lines.join(separator));
+
+    output_lines
+}
