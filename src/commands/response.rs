f8a587a1cc5d5906c805e20778b8964fe38874c9
ilotterytea <me@ilotterytea.kz>
2023-12-30 18:10:41 +0600

    feat: i finally finished ResponseError

--- src/commands/response.rs
+++ src/commands/response.rs
@@ -1,4 +1,8 @@
-use crate::localization::LineId;
+use std::sync::Arc;
+
+use crate::localization::{LineId, Localizator};
+
+use super::{request::Request, CommandArgument};
 
 pub enum Response {
     Single(String),
@@ -7,7 +11,13 @@ pub enum Response {
 
 #[derive(Debug)]
 pub enum ResponseError {
-    NotEnoughArguments,
+    NotEnoughArguments(CommandArgument),
+    WrongArgumentType(String, String),
+    IncorrectArgument(String),
+
+    IncompatibleName(String),
+    NamesakeCreation(String),
+
     NoSubcommand,
     NoMessage,
     UnknownSubcommand,
@@ -18,6 +28,72 @@ pub enum ResponseError {
 
     WrongArguments,
     ExternalAPIError(u32, Option<String>),
+    UserNotFound(String),
+    InsufficientRights,
 
     Custom(LineId),
+}
+
+impl ResponseError {
+    pub fn formatted_message(&self, request: &Request, localizator: Arc<Localizator>) -> String {
+        let hint_url = format!("hint.url.{}", request.command_id);
+        let hint_url_localed = if let Some(line_id) = LineId::from_string(hint_url) {
+            localizator
+                .get_literal_text(request.channel_preference.language.as_str(), line_id)
+                .unwrap_or_default()
+        } else {
+            "".to_string()
+        };
+
+        let docs_line = localizator.formatted_text_by_request(
+            &request,
+            LineId::MsgHint,
+            vec![hint_url_localed],
+        );
+
+        let mut params: Vec<String> = Vec::new();
+
+        let error_line_id: (u8, LineId) = match self {
+            Self::NotEnoughArguments(arg) => (0, arg.to_line_id()),
+            Self::WrongArgumentType(arg, pos) => {
+                params.push(arg.clone());
+                params.push(pos.clone());
+                (1, LineId::ErrorWrongArgumentType)
+            }
+            Self::IncorrectArgument(arg) => {
+                params.push(arg.clone());
+                (2, LineId::ErrorIncorrectArgument)
+            }
+
+            Self::InsufficientRights => (3, LineId::ErrorInsufficientRights),
+            Self::IncompatibleName(arg) => {
+                params.push(arg.clone());
+                (10, LineId::ErrorIncompatibleName)
+            }
+            Self::NamesakeCreation(arg) => {
+                params.push(arg.clone());
+                (11, LineId::ErrorNamesakeCreation)
+            }
+            Self::UserNotFound(arg) => {
+                params.push(arg.clone());
+                (12, LineId::ErrorUserNotFound)
+            }
+            Self::ExternalAPIError(code, reason) => {
+                params.push(code.to_string());
+                params.push(reason.clone().unwrap_or_default());
+                (20, LineId::ErrorExternalAPIError)
+            }
+            _ => (127, LineId::ErrorSomethingWentWrong),
+        };
+
+        let error_line = localizator.formatted_text_by_request(&request, error_line_id.1, params);
+
+        let error = localizator.formatted_text_by_request(
+            &request,
+            LineId::MsgError,
+            vec![error_line_id.0.to_string(), error_line, docs_line],
+        );
+
+        error
+    }
 }
