d6e42bcc7e5c9bf64f40cd19b19db48c41cc41a3
ilotterytea <me@ilotterytea.kz>
2024-01-03 19:44:50 +0600

    feat: actix_web example

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let (host, port) = ("127.0.0.1", 8080);
+    println!("Running the API server at {}:{}", host, port);
+
+    HttpServer::new(|| App::new())
+        .bind((host, port))?
+        .run()
+        .await
 }
