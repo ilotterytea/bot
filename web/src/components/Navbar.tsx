1e9dafa36c2dbd9c9dff2793439f5e690e3e4be2
ilotterytea <me@ilotterytea.kz>
2024-01-07 18:08:24 +0600

    upd: navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,41 +1,54 @@
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 const Navbar = () : JSX.Element => (
-    <div className="flex justify-between items-center h-9 bg-gray-100 border-solid border-slate-300 border-b-2 font-mono text-sm tracking-wider">
+    <div className="w-full h-16 flex justify-around items-center text-lg fixed dark:bg-neutral-900 dark:bg-opacity-50 backdrop-blur-xl">
         {
-        // Logo
+            // Logo
         }
-        <div className="flex items-center h-full">
-            <a href="/" className="flex items-center mx-1">
-            <div className="w-5 mr-1">
-                <Image
-                    src="/tea.png"
-                    width={0}
-                    height={0}
-                    style={{width: '100%', height: 'auto'}}
-                    alt="Logo"
-                />
-            </div>
-            <h1 className="font-mono text-teal-500 font-semibold">
-                iLotterytea's Twitch bot
-            </h1>
-            </a>
+        <div className="flex h-full p-2">
+            <Image
+                alt={"@imteabot"}
+                src={"/bot_avatar.png"}
+                width={128}
+                height={128}
+
+                className="rounded-lg h-full w-fit"
+            />
         </div>
+
         {
-        // Buttons
+            // Quick buttons
         }
-        <div className="flex items-center flex-row h-full">
-            <a href="/cmd" className="flex items-center border-slate-300 border-x-2 px-4 h-full">
-                Commands
+        <div className="flex h-full space-x-6 items-center">
+            <NavbarButton stylized_name="Home" url="/" />
+            <NavbarButton stylized_name="FAQ" url="/faq" />
+            <NavbarButton stylized_name="Wiki" url="/wiki" />
+            <NavbarButton stylized_name="Statistics" url="/stats" />
+        </div>
+
+        {
+            // Log in
+        }
+        <div className="flex h-full items-center">
+            <a href="/login" >
+                <div className="transition px-4 py-2 rounded-full hover:bg-teal-50 text-teal-800 dark:text-teal-200 hover:text-teal-950">
+                    <p><FontAwesomeIcon
+                            icon={faRightFromBracket}
+                        /> Log in...</p>
+                </div>
             </a>
-            <a href="/login" className="flex items-center border-slate-300 border-x-2 px-4 h-full">
-                Log in...
-            </a>
-            <form action={"/search"} style={{height: '100%', width: 'auto'}}>
-                <input name={"query"} placeholder="Search user..." style={{height: '100%', width: 'auto'}}/>
-            </form>
         </div>
     </div>
+);
+
+const NavbarButton = ({stylized_name, url}: {stylized_name: string, url: string}): JSX.Element => (
+    <a href={url} >
+        <div className="transition px-4 py-2 rounded-full hover:bg-neutral-50 dark:hover:bg-neutral-900 text-neutral-600 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-neutral-50">
+            {stylized_name}
+        </div>
+    </a>
 );
 
 export default Navbar;
