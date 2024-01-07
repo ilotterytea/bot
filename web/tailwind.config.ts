ddc55838be70ae1fa19cf5ea49a15634cb256c7e
ilotterytea <me@ilotterytea.kz>
2024-01-07 20:22:02 +0600

    feat: animation for / page

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'mainscreen': 'mainscreen 10s ease infinite'
+      },
+      keyframes: {
+        'mainscreen': {
+          '0%, 100%': {
+            'background-size':'200% 200%',
+             'background-position': 'top center'
+          },
+          '50%': {
+              'background-size':'200% 200%',
+              'background-position': 'bottom center'
+          }
+        }
+      }
     },
   },
   plugins: [],
