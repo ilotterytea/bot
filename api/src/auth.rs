267f76c41011be4087f09d4a72a7cf4604b9f12f
ilotterytea <me@ilotterytea.kz>
2024-01-12 00:23:36 +0600

    feat: generate authentication url

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -2,12 +2,13 @@ use actix_web::{web, HttpResponse};
 use chrono::{Duration, NaiveDateTime, Utc};
 use common::config::Configuration;
 use common::establish_connection;
-use common::models::Session;
+use common::models::{NewSessionState, Session};
 use common::{
     models::{NewSession, NewUser, SessionState, User},
     schema::{session_states::dsl as ss, sessions::dsl as se, users::dsl as us},
 };
 use diesel::{delete, insert_into, ExpressionMethods, QueryDsl, RunQueryDsl};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use twitch_api::twitch_oauth2::{AccessToken, UserToken};
 
@@ -39,6 +40,7 @@ pub async fn authenticate(
     config: web::Data<Configuration>,
     success: Option<web::Query<AuthenticationSuccess>>,
     fail: Option<web::Query<AuthenticationFail>>,
+    request: Option<web::Json<AuthenticationRequest>>,
 ) -> HttpResponse {
     if let Some(success) = success {
         return authenticate_success(config, success).await;
@@ -46,6 +48,10 @@ pub async fn authenticate(
 
     if let Some(fail) = fail {
         return authenticate_fail(fail).await;
+    }
+
+    if let Some(request) = request {
+        return generate_authentication(config, request).await;
     }
 
     HttpResponse::BadRequest().json(Response {
@@ -218,5 +224,78 @@ pub async fn authenticate_fail(query: web::Query<AuthenticationFail>) -> HttpRes
             query.error, query.error_description
         )),
         data: None::<AuthenticationResponse>,
+    })
+}
+
+#[derive(Deserialize)]
+pub struct AuthenticationRequest {
+    pub scopes: Vec<String>,
+}
+
+#[derive(Serialize)]
+pub struct AuthenticationRequestRes {
+    pub url: String,
+    pub state_expires_in: u32,
+}
+
+pub async fn generate_authentication(
+    config: web::Data<Configuration>,
+    req: web::Json<AuthenticationRequest>,
+) -> HttpResponse {
+    let twitch_app_credentials = match config.credentials.twitch_app.clone() {
+        Some(v) => v,
+        None => {
+            return HttpResponse::InternalServerError().json(Response {
+                status_code: 500,
+                message: Some("Secrets for Twitch API are not set on this instance".into()),
+                data: None::<AuthenticationRequestRes>,
+            });
+        }
+    };
+
+    let conn = &mut establish_connection();
+    const STATE_LENGTH: usize = 64;
+    const STATE_CHAR_POOL: &[u8] = b"ABCDEFabcdef0123456789";
+
+    let mut state = String::with_capacity(STATE_LENGTH);
+    let mut rng = rand::thread_rng();
+
+    loop {
+        for _ in 0..STATE_LENGTH {
+            state.push(STATE_CHAR_POOL[rng.gen::<usize>() % STATE_CHAR_POOL.len()] as char);
+        }
+
+        if let Err(_) = ss::session_states
+            .find(&state)
+            .get_result::<SessionState>(conn)
+        {
+            break;
+        }
+
+        state.clear();
+    }
+
+    insert_into(ss::session_states)
+        .values([NewSessionState {
+            state: state.clone(),
+        }])
+        .execute(conn)
+        .expect("Failed to put a new session state");
+
+    let url = format!(
+        "https://id.twitch.tv/oauth2/authorize?response_type=code&redirect_uri={}&client_id={}&scope={}&state={}",
+        twitch_app_credentials.redirect_uri,
+        twitch_app_credentials.client_id,
+        req.scopes.join("%20"),
+        state,
+    );
+
+    HttpResponse::Ok().json(Response {
+        status_code: 200,
+        message: None,
+        data: Some(AuthenticationRequestRes {
+            url,
+            state_expires_in: 300000,
+        }),
     })
 }
