2ded20bd51be2e927bbf8311b6ca52b600140acb
ilotterytea <me@ilotterytea.kz>
2024-01-12 19:40:04 +0600

    feat: cookies for client components

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { CookiesProvider } from 'next-client-cookies/server';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <CookiesProvider>{children}</CookiesProvider>
+      </body>
     </html>
   )
 }
