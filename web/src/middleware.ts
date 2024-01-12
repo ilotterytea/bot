46064e7629345a2a80c561500fcfb464947c84ad
ilotterytea <me@ilotterytea.kz>
2024-01-12 21:44:40 +0600

    feat: middleware checks user authentication

---
+++ web/src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+    const cookies = request.cookies;
+
+    const token = cookies.get("token");
+    const client_id = cookies.get("client_id");
+
+    if (!token || !client_id) {
+        return NextResponse.rewrite(new URL("/login", request.url));
+    }
+
+    let response = NextResponse.next();
+
+    let r = await fetch(
+        "https://api.twitch.tv/helix/users",
+        {
+            headers: {
+                "Authorization": "Bearer " + token.value,
+                "Client-Id": client_id.value
+            }
+        }
+    );
+
+    let j = await r.json();
+
+    // maybe it can be done better
+    response.cookies.set("twitch_user_data", JSON.stringify(j));
+
+    return response;
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"]
+}
