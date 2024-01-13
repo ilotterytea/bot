f487d7517c6e28e2bb83b1b2463d7ccd3c10d2f7
ilotterytea <me@ilotterytea.kz>
2024-01-13 23:26:40 +0600

    feat: login button is now fetching user data

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,47 +1,117 @@
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, User } from "@nextui-org/react";
+import { useCookies } from "next-client-cookies";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
-const Navbar = () : JSX.Element => (
-    <div className="w-full h-16 flex justify-around items-center text-lg fixed dark:bg-neutral-900 dark:bg-opacity-50 backdrop-blur-xl">
-        {
-            // Logo
+const Navbar = () : JSX.Element => {
+    const cookies = useCookies();
+
+    const [channels, setChannels] = useState([]);
+    const [channelIndex, setChannelIndex] = useState(0);
+    const loaded = useRef(false);
+
+    useEffect(() => {
+       if (!loaded.current) {
+        const moderated_channels = cookies.get("ttv_moderated_channels");
+        const moderating_channel = cookies.get("ttv_moderating_index");
+
+        if (moderated_channels && moderating_channel) {
+            setChannels(JSON.parse(moderated_channels));
+            setChannelIndex(JSON.parse(moderating_channel));
+
+            loaded.current = true;
         }
-        <div className="flex h-full p-2">
-            <Image
-                alt={"@imteabot"}
-                src={"/bot_avatar.png"}
-                width={128}
-                height={128}
+       }
+    });
 
-                className="rounded-lg h-full w-fit"
-            />
-        </div>
+    return (
+        <div className="w-full h-16 flex justify-around items-center text-lg fixed dark:bg-neutral-900 dark:bg-opacity-50 backdrop-blur-xl">
+            {
+                // Logo
+            }
+            <div className="flex h-full p-2">
+                <Image
+                    alt={"@imteabot"}
+                    src={"/bot_avatar.png"}
+                    width={128}
+                    height={128}
 
-        {
-            // Quick buttons
-        }
-        <div className="flex h-full space-x-6 items-center">
-            <NavbarButton stylized_name="Home" url="/" />
-            <NavbarButton stylized_name="FAQ" url="/faq" />
-            <NavbarButton stylized_name="Wiki" url="/wiki" />
-            <NavbarButton stylized_name="Statistics" url="/stats" />
-        </div>
+                    className="rounded-lg h-full w-fit"
+                />
+            </div>
 
-        {
-            // Log in
-        }
-        <div className="flex h-full items-center">
-            <a href="/login" >
-                <div className="transition px-4 py-2 rounded-full hover:bg-teal-50 text-teal-800 dark:text-teal-200 hover:text-teal-950">
-                    <p><FontAwesomeIcon
-                            icon={faRightFromBracket}
-                        /> Log in...</p>
-                </div>
-            </a>
+            {
+                // Quick buttons
+            }
+            <div className="flex h-full space-x-6 items-center">
+                <NavbarButton stylized_name="Home" url="/" />
+                <NavbarButton stylized_name="FAQ" url="/faq" />
+                <NavbarButton stylized_name="Wiki" url="/wiki" />
+                <NavbarButton stylized_name="Statistics" url="/stats" />
+            </div>
+
+            {
+                // Log in
+            }
+            <div className="flex h-full items-center">
+                {
+                    !loaded.current ? (
+                        <a href="/login" >
+                            <div className="transition px-4 py-2 rounded-full hover:bg-teal-50 text-teal-800 dark:text-teal-200 hover:text-teal-950">
+                                <p><FontAwesomeIcon
+                                        icon={faRightFromBracket}
+                                    /> Log in...</p>
+                            </div>
+                        </a>
+                    ) :
+                    (
+                        <Dropdown>
+                            <DropdownTrigger>
+                                <Avatar
+                                    src={channels[channelIndex].profile_image_url}
+                                    radius="full"
+                                    size="md"
+                                />
+                            </DropdownTrigger>
+                            <DropdownMenu>
+                                <DropdownSection title={"Signed in as " + channels[channelIndex].login} showDivider={true}>
+                                    <DropdownItem title="Dashboard" href="/dashboard" />
+                                </DropdownSection>
+                                {
+                                    !channels || channels.length === 1 ?
+                                    (<></>) :
+                                    (
+                                        <DropdownSection showDivider={true}>
+                                            {
+                                                channels.filter((_, index) => index !== channelIndex).map((value, index) => (
+                                                    <DropdownItem key={index}>
+                                                        <User
+                                                            name={value.login}
+                                                            avatarProps={{
+                                                                src: value.profile_image_url,
+                                                                size: "sm"
+                                                            }}
+                                                        />
+                                                    </DropdownItem>
+                                                ))
+                                            }
+                                        </DropdownSection>
+                                    )
+                                }
+                                <DropdownSection>
+                                    <DropdownItem title={"Settings"} />
+                                    <DropdownItem title={"Log out"} color="danger"/>
+                                </DropdownSection>
+                            </DropdownMenu>
+                        </Dropdown>
+                    )
+                }
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 const NavbarButton = ({stylized_name, url}: {stylized_name: string, url: string}): JSX.Element => (
     <a href={url} >
