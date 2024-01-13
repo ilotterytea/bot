d2b13512d01e9cabaa5b97738bfe6dab7955781a
ilotterytea <me@ilotterytea.kz>
2024-01-13 23:25:23 +0600

    feat: get moderated channels

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -12,13 +12,15 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
     let response = NextResponse.next();
 
+    const headers = {
+        "Authorization": "Bearer " + token.value,
+        "Client-Id": client_id.value
+    };
+
     let r = await fetch(
         "https://api.twitch.tv/helix/users",
         {
-            headers: {
-                "Authorization": "Bearer " + token.value,
-                "Client-Id": client_id.value
-            }
+            headers: headers
         }
     );
 
@@ -27,6 +29,49 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
     // maybe it can be done better
     response.cookies.set("twitch_user_data", JSON.stringify(user));
+
+    let rr = await fetch(
+        "https://api.twitch.tv/helix/moderation/channels?user_id=" + user.id,
+        {
+            headers: headers
+        }
+    );
+
+    let jj = await rr.json();
+    let channel_ids: any[] = jj.data;
+
+
+    if (channel_ids.length !== 0) {
+        const bot_response = await fetch("http://0.0.0.0:8085/v1/channels");
+        const bot_json = await bot_response.json();
+        const bot_channels: any[] = bot_json.data;
+
+        let channels = [
+            user.id
+        ];
+
+        // i dont care
+        for (const channel_id of channel_ids) {
+            for (const bot_channel of bot_channels) {
+                if (bot_channel.alias_id == channel_id.broadcaster_id) {
+                    channels.push(channel_id.broadcaster_id);
+                }
+            }
+        }
+
+        const users_response = await fetch(
+            "https://api.twitch.tv/helix/users?id=" + channels.join("&id="),
+            {
+                headers: headers
+            }
+        );
+
+        const users_json = await users_response.json();
+        const users_data = users_json.data;
+
+        response.cookies.set("ttv_moderated_channels", JSON.stringify(users_data));
+        response.cookies.set("ttv_moderating_index", "0");
+    }
 
     return response;
 }
