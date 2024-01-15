e365498951c2809279f277fa152c6807e81d797f
ilotterytea <me@ilotterytea.kz>
2024-01-15 15:46:06 +0600

    upd: async way no longer works + NextUI skeletons 💀 💀 💀

--- web/src/app/wiki/page.tsx
+++ web/src/app/wiki/page.tsx
@@ -2,15 +2,57 @@
 
 import Footer from "@/components/Footer";
 import AppNavbar from "@/components/Navbar";
-import { MDXRemote } from "next-mdx-remote/rsc";
+import { Skeleton } from "@nextui-org/react";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import { useEffect, useRef, useState } from "react";
 
-export default async function Page() {
-    return await wikiPage("README");
+export default function Page() {
+    return wikiPage("README");
 }
 
-export async function wikiPage(name: string): Promise<JSX.Element> {
-    const summary_md = await getDocContent("summary");
-    const readme_md = await getDocContent(name);
+export function wikiPage(name: string): JSX.Element {
+    const [summary, setSummary] = useState();
+    const [content, setContent] = useState();
+    const summaryLoaded = useRef(false);
+    const contentLoaded = useRef(false);
+
+    useEffect(() => {
+        const serializeSource = async (str: string) => {
+            return await serialize(str, {
+                mdxOptions: {
+                    development: process.env.NODE_ENV === "development"
+                }
+            });
+        }
+
+        if (!summaryLoaded.current) {
+            fetch("http://0.0.0.0:8085/v1/docs/summary")
+                .then((response) => response.json())
+                .then((json) => {
+
+                    serializeSource(json.data.content)
+                        .then((data) => {
+                            setSummary(data);
+                            summaryLoaded.current = true;
+                        });
+                })
+                .catch((err) => console.error(err));
+        }
+
+        if (!contentLoaded.current) {
+            fetch("http://0.0.0.0:8085/v1/docs/" + name)
+                .then((response) => response.json())
+                .then((json) => {
+                    serializeSource((json.status_code !== 200) ? json.message : json.data.content)
+                        .then((data) => {
+                            setContent(data);
+                            contentLoaded.current = true;
+                        });
+                })
+                .catch((err) => console.error(err));
+        }
+    }, []);
 
     return (
         <main className="w-full min-h-screen flex flex-col">
@@ -18,34 +60,44 @@ export async function wikiPage(name: string): Promise<JSX.Element> {
             <div className="w-full h-full flex flex-row">
                 {
                     // Sidebar
+                    summaryLoaded.current
+                    ? (
+                        <div className="sidebar">
+                            <MDXRemote {...summary} components={{}} />
+                        </div>
+                    )
+                    : (
+                        <Skeleton className="min-h-screen w-[25%] p-4 mb-5">
+                            <div className="h-12 w-full rounded-lg bg-default-300" />
+                        </Skeleton>
+                    )
                 }
-                <div className="sidebar">
-                    <MDXRemote source={summary_md} />
-                </div>
-                {
-                    // Main content
-                }
+                
                 <div className="wiki-content flex justify-center">
                     <div className="w-full xl:w-[75%]">
-                        <MDXRemote source={readme_md} />
+                    {
+                        // Main content
+                        contentLoaded.current
+                        ? (
+                            <MDXRemote {...content} components={{}} />
+                        )
+                        : (
+                            <div className="space-y-2">
+                                <Skeleton className="rounded-lg mb-5">
+                                    <div className="h-12 w-full rounded-lg bg-default-300" />
+                                </Skeleton>
+                                {Array.from({length: 25}).map((_, i) => (
+                                    <Skeleton className="rounded-lg" key={i}>
+                                        <div className="h-6 w-full rounded-lg bg-default-300" />
+                                    </Skeleton>
+                                ))}
+                            </div>
+                        )
+                    }
                     </div>
                 </div>
             </div>
             <Footer />
         </main>
     );
-}
-
-export async function getDocContent(name: string): Promise<string> {
-    const response = await fetch("http://0.0.0.0:8085/v1/docs/" + name);
-
-    const response_json = await response.json();
-
-    if (response_json.status_code != 200) {
-        return response_json.message;
-    }
-
-    const content: string = response_json.data.content;
-
-    return content;
 }
