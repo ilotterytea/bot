6080dc9c2c7ae6fd7a100618e954406ba80583a3
ilotterytea <me@ilotterytea.kz>
2024-01-15 16:07:34 +0600

    upd: added "wiki" link + deleted unreachable code

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -35,100 +35,21 @@ const AppNavbar = () : JSX.Element => {
                     radius="lg"
                 />
             </NavbarBrand>
+
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
                     <Link href={"/"}>
                         Home
                     </Link>
                 </NavbarItem>
+                <NavbarItem>
+                    <Link href={"/wiki"}>
+                        Wiki
+                    </Link>
+                </NavbarItem>
             </NavbarContent>
+
         </Navbar>
-    );
-
-    return (
-        <div className="w-full h-16 flex justify-around items-center text-lg fixed dark:bg-neutral-900 dark:bg-opacity-50 backdrop-blur-xl">
-            {
-                // Logo
-            }
-            <div className="flex h-full p-2">
-                
-            </div>
-
-            {
-                // Quick buttons
-            }
-            <div className="flex h-full space-x-6 items-center">
-                <NavbarButton stylized_name="Home" url="/" />
-                <NavbarButton stylized_name="FAQ" url="/faq" />
-                <NavbarButton stylized_name="Wiki" url="/wiki" />
-                <NavbarButton stylized_name="Statistics" url="/stats" />
-            </div>
-
-            {
-                // Log in
-            }
-            <div className="flex h-full items-center">
-                {
-                    !loaded.current ? (
-                        <a href="/login" >
-                            <div className="transition px-4 py-2 rounded-full hover:bg-teal-50 text-teal-800 dark:text-teal-200 hover:text-teal-950">
-                                <p><FontAwesomeIcon
-                                        icon={faRightFromBracket}
-                                    /> Log in...</p>
-                            </div>
-                        </a>
-                    ) :
-                    (
-                        <Dropdown>
-                            <DropdownTrigger>
-                                <Avatar
-                                    src={channels[channelIndex].profile_image_url}
-                                    radius="full"
-                                    size="md"
-                                />
-                            </DropdownTrigger>
-                            <DropdownMenu>
-                                <DropdownSection title={"Signed in as " + channels[channelIndex].login} showDivider={true}>
-                                    <DropdownItem title="Dashboard" href="/dashboard" />
-                                </DropdownSection>
-                                {
-                                    !channels || channels.length === 1 ?
-                                    (<></>) :
-                                    (
-                                        <DropdownSection showDivider={true}>
-                                            {
-                                                channels.map((value, index) => 
-                                                index !== channelIndex ?
-                                                (<DropdownItem key={index}>
-                                                    <User
-                                                        name={value.login}
-                                                        avatarProps={{
-                                                            src: value.profile_image_url,
-                                                            size: "sm"
-                                                        }}
-                                                        onClick={e => {
-                                                            setChannelIndex(index);
-                                                            cookies.set("ttv_moderating_index", index.toString());
-                                                        }}
-                                                    />
-                                                </DropdownItem>)
-                                                // uuuuh
-                                                : (<DropdownItem className="hidden" />)
-                                                )
-                                            }
-                                        </DropdownSection>
-                                    )
-                                }
-                                <DropdownSection>
-                                    <DropdownItem title={"Settings"} />
-                                    <DropdownItem title={"Log out"} color="danger"/>
-                                </DropdownSection>
-                            </DropdownMenu>
-                        </Dropdown>
-                    )
-                }
-            </div>
-        </div>
     );
 }
 
