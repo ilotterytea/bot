6be00e85ba697c8d14a1b789a0d5d6b76fcc620c
ilotterytea <me@ilotterytea.kz>
2024-01-17 15:44:12 +0600

    feat: database connection in Config struct

--- common/src/config.rs
+++ common/src/config.rs
@@ -5,6 +5,7 @@ pub const BOT_CONFIGURATION_FILE: &str = include_str!("../../Bot.toml");
 #[derive(Deserialize, Default, Debug, Clone)]
 pub struct Configuration {
     pub credentials: Credentials,
+    pub database_connection: DatabaseConnection,
 }
 
 #[derive(Deserialize, Default, Debug, Clone)]
@@ -19,4 +20,12 @@ pub struct TwitchCredentials {
     pub client_id: String,
     pub client_secret: String,
     pub redirect_uri: String,
+}
+
+#[derive(Deserialize, Default, Debug, Clone)]
+pub struct DatabaseConnection {
+    pub username: String,
+    pub password: String,
+    pub hostname: String,
+    pub database_name: String,
 }
