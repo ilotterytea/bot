577bcd0db1d288e86e33861baccfe5da2fa801f2
ilotterytea <me@ilotterytea.kz>
2024-02-29 23:48:36 +0600

    feat: new design for main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,173 +4,183 @@ import { ReactNode } from 'react';
 import ChatBox from '@/components/ChatEmulation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faBell, faClock, faKeyboard, faMessage } from '@fortawesome/free-solid-svg-icons';
-import Footer from '@/components/Footer';
-import AppNavbar from '@/components/Navbar';
+import { faBell, faClock, faKeyboard, faMessage, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <main>
-      <AppNavbar />
-      <div className="w-full min-h-screen flex flex-col text-lg space-y-64">
-        {
-          // Join promotion
-        }
-        <div className="w-full min-h-screen flex flex-col justify-center items-center pb-24 animate-mainscreen bg-gradient-to-b from-teal-200 to-teal-300">
-          <div className="flex flex-col justify-center items-center text-slate-900">
-            <h1 className="text-7xl font-medium my-4">Enhance your chat</h1>
-            <p>A multi-utility Twitch chat bot that brings a lot of functionality into your chat.</p>
-          </div>
-          <div className="mt-24">
-            <a href="/join">
-              <div className="transition px-12 py-2 rounded-full bg-slate-900 hover:bg-teal-50 text-teal-400 hover:text-slate-800">
-                <p className="text-2xl">Join</p>
+    <div className="flex flex-col">
+      <div className="flex w-full grow">
+        <div className="grow space-y-10">
+          
+          {
+            // Introduction
+          }
+          <div className="w-full flex flex-row justify-around items-center px-8 py-[96px] bg-gradient-to-t from-green-500 to-teal-200 animate-mainscreen shadow-lg">
+            <div className="flex justify-center items-center">
+              <Image
+                src={"/bot_avatar.png"}
+                width={256}
+                height={256}
+                alt="bot's avatar"
+              />
+            </div>
+            <div className="flex flex-col space-y-8 text-right w-[50%]">
+              <div>
+                <h1 className="text-3xl font-medium [text-shadow:_1px_1px_3px_rgb(0_0_0_/_40%)]">Enhance your chat with <span className="text-rose-800">teabot</span></h1>
+                <p className="text-md [text-shadow:_1px_1px_3px_rgb(0_0_0_/_40%)]">A multi-utility Twitch chat bot that brings<br/>a lot of functionality into your chat.</p>
               </div>
-            </a>
+              <Link href={"/wiki/join"} className="h-[64px]">
+                <Button className="bg-rose-800 text-stone-50 h-full text-lg font-anta rounded-lg shadow-lg [text-shadow:_1px_1px_3px_rgb(0_0_0_/_40%)]">
+                  <FontAwesomeIcon icon={faRightToBracket} /> Add to my chat room
+                </Button>
+              </Link>
+            </div>
           </div>
-        </div>
-
-        {
-          // Big features
-        }
-        
-        <div className="w-full flex flex-col items-center justify-center px-16 lg:px-0 [&>div:nth-child(even)]:flex-row-reverse">
-          <div className="w-full lg:w-1/2 space-y-16">
+          
+          {
+            // Features
+          }
+          <div className="flex flex-col space-y-16 shadow-lg bg-stone-200 dark:bg-stone-800">
             {
-              // Stream notifications
+              // Features header
             }
-
-              <FeatureItem name="Stream notifications" description={
-                <p>
-                A convenient way to keep track of streams on Twitch.
-                Simply use the <a href="/cmd/event" className="command">!event</a> command to create a notification in chat about an upcoming stream.
-                To always be in the know, sign up for notifications using the <a href="/cmd/notify" className="command">!notify</a> command.
-                Never miss a moment and enjoy your streams on time!
-                </p>
-              }>
-                <ChatBox animate={true} lines={[
+            <div className="w-full flex justify-center items-center p-6 bg-stone-300 dark:bg-green-400 font-anta">
+              <h1 className="text-4xl font-semibold text-stone-800 dark:text-green-900 [text-shadow:_1px_1px_3px_rgb(0_0_0_/_40%)]">Features</h1>
+            </div>
+            {
+              // Feature list
+            }
+            <div className="p-8 space-y-32">
+              {
+                // Big features
+              }
+              <div className="w-full flex flex-col px-16 lg:px-0">
+                <div className="w-full space-y-16 [&>div:nth-child(even)]:flex-row-reverse">
                   {
-                    "name": "ImTeaBot",
-                    "msg": "⚡ forsen live!"
-                  },
-                  {
-                    "name": "ImTeaBot",
-                    "msg": "⚡ psp live yippeeeee · @juh, @buh"
-                  },
-                  {
-                    "name": "ImTeaBot",
-                    "msg": "⚡ yo ! · @cuh"
+                    // Stream notifications
                   }
-                ]}
-                />
-              </FeatureItem>
+                    <FeatureItem name="Stream notifications" description={
+                      <p>
+                      A convenient way to keep track of streams on Twitch.
+                      Simply use the <a href="/wiki/stream/events" className="command">!event</a> command to create a notification in chat about an upcoming stream.
+                      To always be in the know, sign up for notifications using the <a href="/wiki/stream/notifications" className="command">!notify</a> command.
+                      Never miss a moment and enjoy your streams on time!
+                      </p>
+                    }>
+                      <ChatBox animate={true} lines={[
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "⚡ forsen live!"
+                        },
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "⚡ psp live yippeeeee · @juh, @buh"
+                        },
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "⚡ yo ! · @cuh"
+                        }
+                      ]}
+                      />
+                    </FeatureItem>
 
-            {
-              // Emote updates
-            }
-
-              <FeatureItem name="Emote updates" description={
-                <p>
-                Instant updates on new changes to the emoji set.
-                The bot automatically notifies the chat about new emotes, deletion of existing emotes, and changes in their names.<br />
-                <span className="font-bold text-amber-500">For now only for 7TV.</span>
-                </p>
-              }>
-                {
-                  // TODO: Represent emotes as image instead of text.
-                }
-                <ChatBox animate={false} lines={[
                   {
-                    "name": "ImTeaBot",
-                    "msg": "(7TV) EmoteExpert added the buh emote"
-                  },
-                  {
-                    "name": "ImTeaBot",
-                    "msg": "(7TV) EmoteExpert updated the emote name from buh to buuuh"
-                  },
-                  {
-                    "name": "ImTeaBot",
-                    "msg": "(7TV) EmoteExpert removed the buuuh emote"
+                    // Emote updates
                   }
 
-                ]}
-                />
-              </FeatureItem>
-          </div>
-        </div>
+                    <FeatureItem name="Emote updates" description={
+                      <p>
+                      Instant updates on new changes to the emoji set.
+                      The bot automatically notifies the chat about new emotes, deletion of existing emotes, and changes in their names.<br />
+                      <span className="font-bold text-amber-700 dark:text-amber-500">For now only for 7TV.</span>
+                      </p>
+                    }>
+                      {
+                        // TODO: Represent emotes as image instead of text.
+                      }
+                      <ChatBox animate={false} lines={[
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "(7TV) EmoteExpert added the buh emote"
+                        },
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "(7TV) EmoteExpert updated the emote name from buh to buuuh"
+                        },
+                        {
+                          "name": "ImTeaBot",
+                          "msg": "(7TV) EmoteExpert removed the buuuh emote"
+                        }
 
-        {
-          // Small features
-        }
-
-        <div className="w-full flex flex-col justify-center items-center">
-          <div className="w-full lg:w-1/2 px-16 lg:px-0 grid grid-cols-2 gap-6">
-            <SmallFeatureItem
-              name="@everyone in your chat"
-              description={(
-              <p>
-                With the "<a href="/cmd/massping" className="command">!massping</a>" command,
-                moderators can quickly notify all participants by mentioning them with an important message.
-              </p>
-              )}
-              icon={(<SmallFeatureIcon icon={faBell} class_name={"from-teal-800 to-teal-600"}/>)}
-            />
-
-            <SmallFeatureItem
-              name="No Ctrl+Enter anymore"
-              description={(
-              <p>
-                In addition, the "<a href="/cmd/spam" className="command">!spam</a>" command provides the ability to repeat a message the desired number of times,
-                which is useful for repetitive reminders or clarifications.
-              </p>
-              )}
-              icon={(<SmallFeatureIcon icon={faKeyboard} class_name={"from-amber-600 to-amber-800"} />)}
-            />
-
-            <SmallFeatureItem
-              name="Timers"
-              description={(
-              <p>
-                With the <a href="/cmd/timer" className="command">!timer</a> command, you can set messages to be sent each interval.
-                moderators can quickly notify all participants by mentioning them with an important message.
-              </p>
-              )}
-              icon={(<SmallFeatureIcon icon={faClock} class_name={"from-cyan-800 to-cyan-600"} />)}
-            />
-
-            <SmallFeatureItem
-              name="Create your own commands"
-              description={(
-              <p>
-                The <a href="/cmd/cmd" className="command">!cmd</a> command allow the creation of personalized commands,
-                which adds interactivity and flexibility to communication by allowing channel owners to create their own commands.
-              </p>
-              )}
-              icon={(<SmallFeatureIcon icon={faMessage} class_name={"from-fuchsia-600 to-fuchsia-800"} />)}
-            />
-
-
-          </div>
-        </div>
-
-        {
-          // Last warning 
-        }
-
-        <div className="w-full flex flex-col justify-center items-center space-y-16">
-          <h1 className="font-semibold text-7xl text-red-950 dark:text-red-100">Last chance...</h1>
-            <a href="/join">
-              <div className="transition px-12 py-2 rounded-full bg-slate-900 hover:bg-red-50 text-red-50 hover:text-slate-800">
-                <p className="text-2xl">Join. Now.</p>
+                      ]}
+                      />
+                    </FeatureItem>
+                </div>
               </div>
-            </a>
+
+              {
+                // Small features
+              }
+
+              <div className="w-full flex flex-col justify-center items-center">
+                <div className="w-full x-16 lg:px-0 grid grid-cols-2 gap-6">
+                  <SmallFeatureItem
+                    name="@everyone in your chat"
+                    description={(
+                    <p>
+                      With the "<a href="/wiki/mod/mass-ping" className="command">!massping</a>" command,
+                      moderators can quickly notify all participants by mentioning them with an important message.
+                    </p>
+                    )}
+                    icon={(<SmallFeatureIcon icon={faBell} class_name={"from-teal-800 to-teal-600"}/>)}
+                  />
+
+                  <SmallFeatureItem
+                    name="No Ctrl+Enter anymore"
+                    description={(
+                    <p>
+                      In addition, the "<a href="/wiki/mod/spam" className="command">!spam</a>" command provides the ability to repeat a message the desired number of times,
+                      which is useful for repetitive reminders or clarifications.
+                    </p>
+                    )}
+                    icon={(<SmallFeatureIcon icon={faKeyboard} class_name={"from-amber-600 to-amber-800"} />)}
+                  />
+
+                  <SmallFeatureItem
+                    name="Timers"
+                    description={(
+                    <p>
+                      With the <a href="/wiki/channel/timer" className="command">!timer</a> command, you can set messages to be sent each interval.
+                      moderators can quickly notify all participants by mentioning them with an important message.
+                    </p>
+                    )}
+                    icon={(<SmallFeatureIcon icon={faClock} class_name={"from-cyan-800 to-cyan-600"} />)}
+                  />
+
+                  <SmallFeatureItem
+                    name="Create your own commands"
+                    description={(
+                    <p>
+                      The <a href="/wiki/channel/custom-commands" className="command">!cmd</a> command allow the creation of personalized commands,
+                      which adds interactivity and flexibility to communication by allowing channel owners to create their own commands.
+                    </p>
+                    )}
+                    icon={(<SmallFeatureIcon icon={faMessage} class_name={"from-fuchsia-600 to-fuchsia-800"} />)}
+                  />
+
+
+                </div>
+              </div>
+            </div>
+            
+          </div>
         </div>
-
-        <Footer />
-
       </div>
-    </main>
-  )
+    </div>
+  );
 }
 
 const FeatureItem = (
@@ -179,7 +189,9 @@ const FeatureItem = (
 ): JSX.Element => (
   <div className="w-full flex flex-row justify-between items-center">
     <div className="px-6">
-      <h1 className="text-4xl font-medium mb-6">{name}</h1>
+      <h1 className="text-4xl font-medium mb-6 dark:text-teal-300">
+        {name}
+      </h1>
       {description}
     </div>
     <div>
@@ -192,11 +204,11 @@ const SmallFeatureItem = (
   {name, description, icon}:
   {name: ReactNode, description: ReactNode, icon: ReactNode}
 ): JSX.Element => (
-  <div className="flex flex-col min-w-80 p-8 space-y-4 bg-neutral-200 dark:bg-zinc-800 rounded-2xl border-2 border-neutral-300 dark:border-neutral-950">
+  <div className="flex flex-col min-w-80 p-8 space-y-4 rounded-lg bg-stone-300 dark:bg-stone-700 border-1 border-stone-300 dark:border-stone-900 shadow-[0_0px_4px_0px_rgba(0,0,0,0.3)]">
     {icon}
-    
-    
-    <h1 className="text-2xl font-medium">{name}</h1>
+    <h1 className="text-2xl font-medium dark:text-teal-200">
+      {name}
+    </h1>
     {description}
     
   </div>
