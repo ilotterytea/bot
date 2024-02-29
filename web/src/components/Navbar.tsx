aadc031cc82fd94b56c5f4465f080e9e42724f7d
ilotterytea <me@ilotterytea.kz>
2024-02-29 22:43:46 +0600

    feat: new navigation bar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,181 +1,37 @@
-import { faBook, faChevronDown, faHand, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Avatar, Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Navbar, NavbarBrand, NavbarContent, NavbarItem, User } from "@nextui-org/react";
-import { useCookies } from "next-client-cookies";
-import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
 
-const AppNavbar = () : JSX.Element => {
-    const cookies = useCookies();
+/**
+ * @deprecated Use NavigationBar
+ */
+export function AppNavbar(): JSX.Element {
+    return (<></>);
+}
 
-    const [channels, setChannels] = useState([]);
-    const [channelIndex, setChannelIndex] = useState(0);
-    const loaded = useRef(false);
-
-    useEffect(() => {
-       if (!loaded.current) {
-        const moderated_channels = cookies.get("ttv_moderated_channels");
-        const moderating_channel = cookies.get("ttv_moderating_index");
-
-        if (moderated_channels && moderating_channel) {
-            setChannels(JSON.parse(moderated_channels));
-            setChannelIndex(JSON.parse(moderating_channel));
-
-            loaded.current = true;
-        }
-       }
-    });
-
-    const navbarItems = [
+export function NavigationBar(): JSX.Element {
+    const items = [
         {
-            "name": "Wiki",
-            "icon": faBook,
-            "url": "/wiki"
+            name: "home",
+            url: "/"
         },
         {
-            "name": "Add to a channel",
-            "icon": faHand,
-            "url": "/join"
+            name: "wiki",
+            url: "/wiki"
         }
     ];
 
     return (
-        <Navbar isBordered classNames={{
-            wrapper: "justify-start",
-            brand: "grow-0",
-        }}>
-            <NavbarBrand>
-                <Avatar
-                    src={"/bot_avatar.png"}
-                    radius="lg"
-                />
-            </NavbarBrand>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                {
-                    channels.length > 0 ?
-                    (
-                        <>
-                            <NavbarItem>
-                                {
-                                    channels[channelIndex].internal_data ?
-                                    (
-                                        <Link href={"/dashboard"}>Dashboard</Link>
-                                    )
-                                    :
-                                    (
-                                        <Link href={"/join"}>Join channel</Link>
-                                    )
-                                }
-                            </NavbarItem>
-                            <Dropdown placement="bottom-start">
-                                <DropdownTrigger>
-                                    <Button>More <FontAwesomeIcon icon={faChevronDown} /></Button>
-                                </DropdownTrigger>
-                                <DropdownMenu aria-label="More" variant="flat">
-                                    {
-                                        navbarItems.map((v, i) => (
-                                            <DropdownItem key={v.name}>
-                                                <Link href={v.url}><p><FontAwesomeIcon icon={v.icon} /> {v.name}</p></Link>
-                                            </DropdownItem>
-                                        ))
-                                    }
-                                </DropdownMenu>
-                            </Dropdown>
-                        </>
-                    )
-                    :
-                    (
-                        navbarItems.map((v, i) => (
-                            <NavbarItem key={i}>
-                                <Link href={v.url}>{v.name}</Link>
-                            </NavbarItem>
+        <div className="w-full flex justify-center items-center p-4">
+            <div className="w-full lg:w-[50%] flex flex-row">
+                <div className="grow flex flex-row text-sm">
+                    {
+                        items.map((v, i) => (
+                            <Link className="mx-4 my-2 transition-colors hover:text-teal-400" href={v.url} key={i}>
+                                {v.name}
+                            </Link>
                         ))
-                    )
-                }
-            </NavbarContent>
-
-            <NavbarContent as={"div"} justify={"end"}>
-                {
-                    channels.length > 0 ?
-                    (
-                        
-                            <Dropdown placement="bottom-end">
-                                <DropdownTrigger>
-                                    <Avatar
-                                        isBordered
-                                        as={"button"}
-                                        color="primary"
-                                        name={channels[channelIndex].login}
-                                        src={channels[channelIndex].profile_image_url}
-                                    />
-                                </DropdownTrigger>
-                                <DropdownMenu aria-label="Profile Actions" variant="flat">
-                                    {
-                                        channels.length > 1 ?
-                                        (
-                                            <DropdownSection title={"Moderated channels"}>
-                                                {
-                                                    channels.map((v, i) => {
-                                                        return i != channelIndex ?
-                                                        (
-                                                            <DropdownItem key={v.login} onPress={() => {
-                                                                setChannelIndex(i);
-                                                                cookies.set("ttv_moderating_index", i.toString());
-                                                            }}>
-                                                                <User
-                                                                    name={v.login}
-                                                                    avatarProps={{
-                                                                        src: v.profile_image_url,
-                                                                        size: "sm"
-                                                                    }}
-                                                                    
-                                                                />
-                                                            </DropdownItem>
-                                                        )
-                                                        :
-                                                        // uuuuhhhhh
-                                                        (<DropdownItem className="hidden" />);
-                                                    })
-                                                }
-                                            </DropdownSection>
-                                        )
-                                        :
-                                        // omg im so fucking tired of random "cannot convert symbol to string" errors
-                                        // i dont know why <></> is causing this error
-                                        // AND THERE'S NO PROPER REASON FOR IT ON THE INTERNET
-                                        (<DropdownSection className="hidden">
-                                            <DropdownItem className="hidden" />
-                                        </DropdownSection>)
-                                    }
-
-                                    <DropdownSection title={"My account"}>
-                                        <DropdownItem key={"settings"}>
-                                            <Link href={"/settings"}>Settings</Link>
-                                        </DropdownItem>
-                                        <DropdownItem key={"logout"} color="danger">Log out</DropdownItem>
-                                    </DropdownSection>
-                                </DropdownMenu>
-
-                            </Dropdown>
-                    )
-                    :
-                    (<></>)
-                }
-
-            </NavbarContent>
-
-        </Navbar>
-    );
-}
-
-const NavbarButton = ({stylized_name, url}: {stylized_name: string, url: string}): JSX.Element => (
-    <a href={url} >
-        <div className="transition px-4 py-2 rounded-full hover:bg-neutral-50 dark:hover:bg-neutral-900 text-neutral-600 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-neutral-50">
-            {stylized_name}
+                    }
+                </div>
+            </div>
         </div>
-    </a>
-);
-
-export default AppNavbar;
+    );
+};
