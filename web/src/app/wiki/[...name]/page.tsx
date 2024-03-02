9767527ca83afcd3c69e78bcd75ab533a515e466
ilotterytea <me@ilotterytea.kz>
2024-03-02 16:33:37 +0500

    feat: dynamic title for wiki pages

--- web/src/app/wiki/[...name]/page.tsx
+++ web/src/app/wiki/[...name]/page.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from "next";
 import WikiPage from "../wiki";
 
 export default function Page({params}: {params: {name: string[]}}) {
     return <><WikiPage page_id={params.name.join("/")}/></>;
+}
+
+export async function generateMetadata({
+    params
+}: {params: {name: string[]}}): Promise<Metadata> {
+    const { name } = params;
+
+    return {
+        title: name[name.length - 1]
+    }
 }
