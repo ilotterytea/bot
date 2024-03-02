3611869815ab802ea5347e9591dd5a564a03aaf8
ilotterytea <me@ilotterytea.kz>
2024-03-02 16:28:48 +0500

    feat: metadata for wiki homepage

--- web/src/app/wiki/page.tsx
+++ web/src/app/wiki/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import WikiPage from "./wiki";
+
+export const metadata: Metadata = {
+    title: "wiki"
+};
 
 export default function Page() {
     return <><WikiPage page_id="README" /></>;
