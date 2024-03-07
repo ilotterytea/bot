2ba0940a4e505978a384b0d33c61abfd83802ea7
ilotterytea <me@ilotterytea.kz>
2024-03-08 00:52:39 +0500

    feat: notify about stream title changes

--- bot/src/livestream.rs
+++ bot/src/livestream.rs
@@ -1,10 +1,10 @@
-use std::{sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use chrono::{NaiveDateTime, Utc};
 use common::{establish_connection, models::EventType};
 use log::{error, info};
 use twitch_api::{
-    helix::streams::GetStreamsRequest,
+    helix::{channels::GetChannelInformationRequest, streams::GetStreamsRequest},
     types::{UserId, UserIdRef},
 };
 
@@ -14,6 +14,7 @@ pub struct TwitchLivestreamHelper {
     bundle: Arc<InstanceBundle>,
 
     online_channels_cache: Vec<UserId>,
+    title_channel_cache: HashMap<UserId, String>,
 }
 
 impl TwitchLivestreamHelper {
@@ -21,6 +22,7 @@ impl TwitchLivestreamHelper {
         Self {
             bundle,
             online_channels_cache: Vec::new(),
+            title_channel_cache: HashMap::new(),
         }
     }
 
@@ -28,12 +30,13 @@ impl TwitchLivestreamHelper {
         info!("Starting to listen to stream events...");
 
         loop {
-            self.process_channels().await;
+            self.process_live_channels().await;
+            self.process_stream_info().await;
             tokio::time::sleep(Duration::from_secs(5)).await;
         }
     }
 
-    pub async fn process_channels(&mut self) {
+    async fn process_live_channels(&mut self) {
         let data = self.bundle.twitch_livestream_websocket_data.lock().await;
 
         let channel_ids = data
@@ -105,10 +108,64 @@ impl TwitchLivestreamHelper {
 
                     self.online_channels_cache.remove(position);
 
-                    handle_stream_event(conn, self.bundle.clone(), id, EventType::Offline).await;
+                    handle_stream_event(
+                        conn,
+                        self.bundle.clone(),
+                        id,
+                        EventType::Offline,
+                        Vec::new(),
+                    )
+                    .await;
                 }
             }
             Err(e) => error!("Failed to get streams: {:?}", e),
+        }
+    }
+
+    async fn process_stream_info(&mut self) {
+        let data = self.bundle.twitch_livestream_websocket_data.lock().await;
+
+        let channel_ids = data
+            .iter()
+            .map(|x| UserIdRef::from_str(x.as_str()))
+            .collect::<Vec<&UserIdRef>>();
+
+        if channel_ids.is_empty() {
+            return;
+        }
+
+        let request = GetChannelInformationRequest::broadcaster_ids(channel_ids);
+
+        match self
+            .bundle
+            .twitch_api_client
+            .req_get(request, &*self.bundle.twitch_api_token)
+            .await
+        {
+            Ok(v) => {
+                let conn = &mut establish_connection();
+
+                let channels = v.data;
+
+                for channel in channels {
+                    if let Some(old_title) = self
+                        .title_channel_cache
+                        .insert(channel.broadcaster_id.clone(), channel.title.clone())
+                    {
+                        if old_title.ne(&channel.title) {
+                            handle_stream_event(
+                                conn,
+                                self.bundle.clone(),
+                                channel.broadcaster_id.clone(),
+                                EventType::Title,
+                                vec![old_title, channel.title.clone()],
+                            )
+                            .await;
+                        }
+                    }
+                }
+            }
+            Err(e) => error!("Failed to get channel infos: {:?}", e),
         }
     }
 }
