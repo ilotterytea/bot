2f998a8ab210d85e6ae5430139e9deb3dfdb3d44
ilotterytea <me@ilotterytea.kz>
2024-04-21 00:31:05 +0500

    feat: join vector util

--- src/utils/string.hpp
+++ src/utils/string.hpp
@@ -4,3 +4,6 @@
 #include <vector>
 
 std::vector<std::string> split_text(const std::string &text, char delimiter);
+
+std::string join_vector(const std::vector<std::string> &vec, char delimiter);
+std::string join_vector(const std::vector<std::string> &vec);
