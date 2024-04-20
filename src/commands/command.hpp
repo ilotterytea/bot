b4fca15aec26332c1315c8c3030169bbf342da3e
ilotterytea <iltsu@alright.party>
2024-04-21 03:25:12 +0500

    feat: command loader

--- src/commands/command.hpp
+++ src/commands/command.hpp
@@ -1,3 +1,7 @@
+#pragma once
+
+#include <memory>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -7,9 +11,23 @@
 namespace bot {
   namespace command {
     class Command {
+      public:
         virtual std::string get_name() = 0;
         virtual std::variant<std::vector<std::string>, std::string> run(
             const irc::Message<irc::MessageType::Privmsg> &msg) = 0;
+    };
+
+    class CommandLoader {
+      public:
+        CommandLoader();
+        ~CommandLoader() = default;
+
+        void add_command(std::unique_ptr<Command> cmd);
+        std::optional<std::variant<std::vector<std::string>, std::string>> run(
+            const irc::Message<irc::MessageType::Privmsg> &msg);
+
+      private:
+        std::vector<std::unique_ptr<Command>> commands;
     };
   }
 }
