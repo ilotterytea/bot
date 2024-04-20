0938bf7f72b251917346c0c3ef698e06529870b6
ilotterytea <iltsu@alright.party>
2024-04-21 00:31:05 +0500

    feat: join vector util

--- src/utils/string.cpp
+++ src/utils/string.cpp
@@ -16,3 +16,25 @@ std::vector<std::string> split_text(const std::string &text, char delimiter) {
 
   return parts;
 }
+
+std::string join_vector(const std::vector<std::string> &vec, char delimiter) {
+  std::string str;
+
+  for (auto i = vec.begin(); i != vec.end() - 1; i++) {
+    str += *i + delimiter;
+  }
+
+  str += vec[vec.size() - 1];
+
+  return str;
+}
+
+std::string join_vector(const std::vector<std::string> &vec) {
+  std::string str;
+
+  for (const auto &e : vec) {
+    str += e;
+  }
+
+  return str;
+}
