292f8fa887cc69219e96b51142ced68056b1e5e3
ilotterytea <me@ilotterytea.kz>
2024-04-30 01:16:41 +0500

    feat: create a new db connection for each message and pass work to it

--- src/handlers.hpp
+++ src/handlers.hpp
@@ -3,10 +3,12 @@
 #include "bundle.hpp"
 #include "commands/command.hpp"
 #include "irc/message.hpp"
+#include "pqxx/pqxx"
 
 namespace bot::handlers {
   void handle_private_message(
       const InstanceBundle &bundle,
       const command::CommandLoader &command_loader,
-      const irc::Message<irc::MessageType::Privmsg> &message);
+      const irc::Message<irc::MessageType::Privmsg> &message,
+      const pqxx::work &work);
 }
