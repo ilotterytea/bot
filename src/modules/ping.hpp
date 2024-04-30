846a1f7174dbcf29adf4ca68837dc7e4e4991cce
ilotterytea <me@ilotterytea.kz>
2024-05-01 00:15:19 +0500

    upd: updated the '!ping' command

--- src/modules/ping.hpp
+++ src/modules/ping.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <chrono>
 #include <string>
 #include <variant>
 #include <vector>
 
 #include "../bundle.hpp"
 #include "../commands/command.hpp"
+#include "../utils/chrono.hpp"
 
 namespace bot {
   namespace mod {
@@ -15,7 +21,21 @@ namespace bot {
         std::variant<std::vector<std::string>, std::string> run(
             const InstanceBundle &bundle,
             const command::Request &request) const override {
-          return "pong";
+          auto now = std::chrono::steady_clock::now();
+          auto duration = now - START_TIME;
+          auto seconds =
+              std::chrono::duration_cast<std::chrono::seconds>(duration);
+          std::string uptime = utils::chrono::format_timestamp(seconds.count());
+
+          struct rusage usage;
+          getrusage(RUSAGE_SELF, &usage);
+
+          int used_memory = usage.ru_maxrss / 1024;
+
+          return bundle.localization
+              .get_formatted_line(request, loc::LineId::PingResponse,
+                                  {uptime, std::to_string(used_memory)})
+              .value();
         }
     };
   }
