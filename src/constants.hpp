512126ae78f1beca69600841ab727cd7ba322936
ilotterytea <me@ilotterytea.kz>
2024-05-01 00:12:43 +0500

    feat: START_TIME constant

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <chrono>
 #include <string>
 
 #define DEFAULT_LOCALE_ID "english"
 
 const std::string DEFAULT_PREFIX = "!";
+const auto START_TIME = std::chrono::steady_clock::now();
