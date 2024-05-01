f79711e66b5e7bf347ab229bf0de8e21ab87471f
ilotterytea <me@ilotterytea.kz>
2024-05-02 00:13:10 +0500

    feat: added HelixClient into InstanceBundle

--- src/bundle.hpp
+++ src/bundle.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "api/twitch/helix_client.hpp"
 #include "irc/client.hpp"
 #include "localization/localization.hpp"
 
 namespace bot {
   struct InstanceBundle {
       irc::Client &irc_client;
+      const api::twitch::HelixClient &helix_client;
       const bot::loc::Localization &localization;
   };
 }
