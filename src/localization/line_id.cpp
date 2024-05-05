67c2967e4fbe5661b784566b222d4efbd85fb6b9
ilotterytea <me@ilotterytea.kz>
2024-05-05 15:04:31 +0500

    feat: line ids for command arguments + added missing error line

--- src/localization/line_id.cpp
+++ src/localization/line_id.cpp
@@ -8,7 +8,25 @@ namespace bot {
     std::optional<LineId> string_to_line_id(const std::string &str) {
       if (str == "ping.response") {
         return LineId::PingResponse;
-      } else if (str == "error.template") {
+      }
+
+      else if (str == "argument.subcommand") {
+        return LineId::ArgumentSubcommand;
+      } else if (str == "argument.message") {
+        return LineId::ArgumentMessage;
+      } else if (str == "argument.interval") {
+        return LineId::ArgumentInterval;
+      } else if (str == "argument.name") {
+        return LineId::ArgumentName;
+      } else if (str == "argument.target") {
+        return LineId::ArgumentTarget;
+      } else if (str == "argument.value") {
+        return LineId::ArgumentValue;
+      } else if (str == "argument.amount") {
+        return LineId::ArgumentAmount;
+      }
+
+      else if (str == "error.template") {
         return LineId::ErrorTemplate;
       } else if (str == "error.not_enough_arguments") {
         return LineId::ErrorNotEnoughArguments;
@@ -24,7 +42,9 @@ namespace bot {
         return LineId::ErrorSomethingWentWrong;
       } else if (str == "error.insufficient_rights") {
         return LineId::ErrorInsufficientRights;
-      } else {
+      }
+
+      else {
         return std::nullopt;
       }
     }
