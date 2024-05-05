5521c9599bf2593f4da1c18b383953429c98a740
ilotterytea <me@ilotterytea.kz>
2024-05-06 00:43:06 +0500

    feat: method for converting strings to event types

---
+++ src/schemas/stream.cpp
@@ -0,0 +1,17 @@
+#include "stream.hpp"
+
+namespace bot::schemas {
+  EventType string_to_event_type(const std::string &type) {
+    if (type == "live") {
+      return EventType::LIVE;
+    } else if (type == "offline") {
+      return EventType::OFFLINE;
+    } else if (type == "title") {
+      return EventType::TITLE;
+    } else if (type == "game") {
+      return EventType::GAME;
+    } else {
+      return EventType::CUSTOM;
+    }
+  }
+}
--- src/schemas/stream.hpp
+++ src/schemas/stream.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <string>
+
 namespace bot::schemas {
   enum EventType { LIVE, OFFLINE, TITLE, GAME, CUSTOM = 99 };
+  EventType string_to_event_type(const std::string &type);
+
   enum EventFlag { MASSPING };
+
 }
