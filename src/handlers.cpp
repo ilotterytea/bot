3e488b50c8f369e4d3eec6761bf8dc658669f050
ilotterytea <me@ilotterytea.kz>
2024-05-05 16:24:50 +0500

    feat: handle response exception

--- src/handlers.cpp
+++ src/handlers.cpp
@@ -1,8 +1,8 @@
 #include "handlers.hpp"
 
+#include <exception>
 #include <optional>
 #include <pqxx/pqxx>
-#include <variant>
 #include <vector>
 
 #include "bundle.hpp"
@@ -10,6 +10,7 @@
 #include "commands/request.hpp"
 #include "commands/request_util.hpp"
 #include "irc/message.hpp"
+#include "localization/line_id.hpp"
 
 namespace bot::handlers {
   void handle_private_message(
@@ -21,24 +22,32 @@ namespace bot::handlers {
         command::generate_request(command_loader, message, conn);
 
     if (request.has_value()) {
-      auto o_response = command_loader.run(bundle, request.value());
+      try {
+        auto response = command_loader.run(bundle, request.value());
 
-      if (o_response.has_value()) {
-        auto response = o_response.value();
-
-        try {
-          auto str = std::get<std::string>(response);
-          bundle.irc_client.say(message.source.login, str);
-        } catch (const std::exception &e) {
-        }
-
-        try {
-          auto strs = std::get<std::vector<std::string>>(response);
-          for (const std::string &str : strs) {
+        if (response.has_value()) {
+          try {
+            auto str = std::get<std::string>(*response);
             bundle.irc_client.say(message.source.login, str);
+          } catch (const std::exception &e) {
           }
-        } catch (const std::exception &e) {
+
+          try {
+            auto strs = std::get<std::vector<std::string>>(*response);
+            for (const std::string &str : strs) {
+              bundle.irc_client.say(message.source.login, str);
+            }
+          } catch (const std::exception &e) {
+          }
         }
+      } catch (const std::exception &e) {
+        std::string line =
+            bundle.localization
+                .get_formatted_line(request.value(), loc::LineId::ErrorTemplate,
+                                    {e.what()})
+                .value();
+
+        bundle.irc_client.say(message.source.login, line);
       }
     }
   }
