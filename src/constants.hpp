d7a2de17e9b7931f68b5b4079b1c36866a19d343
ilotterytea <iltsu@alright.party>
2024-05-18 00:12:22 +0500

    upd: use the other prefix if the bot in debug mode

--- src/constants.hpp
+++ src/constants.hpp
@@ -5,5 +5,9 @@
 
 #define DEFAULT_LOCALE_ID "english"
 
+#ifdef DEBUG_MODE
+const std::string DEFAULT_PREFIX = "~";
+#else
 const std::string DEFAULT_PREFIX = "!";
+#endif
 const auto START_TIME = std::chrono::steady_clock::now();
