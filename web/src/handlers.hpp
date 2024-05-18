39635a9a122084c2ebd76926543ad8c6ef172722
ilotterytea <me@ilotterytea.kz>
2024-05-19 00:58:38 +0500

    feat: use cfg to replace placeholders

--- web/src/handlers.hpp
+++ web/src/handlers.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <optional>
+
+#include "config.hpp"
 #include "crow/http_response.h"
 
 namespace botweb {
-  crow::response get_wiki_page(const std::string &path);
+  crow::response get_wiki_page(const std::string &path,
+                               const std::optional<Configuration> &cfg);
 }
