a83917beb806596988762943d3173a0109dc4a4d
ilotterytea <iltsu@alright.party>
2024-05-18 16:52:53 +0500

    feat: mustache templates

--- web/src/main.cpp
+++ web/src/main.cpp
@@ -1,9 +1,15 @@
 #include "crow/app.h"
+#include "crow/mustache.h"
 
 int main(int argc, char *argv[]) {
   crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/")([]() { return "hi"; });
+  CROW_ROUTE(app, "/")
+  ([]() {
+    auto page = crow::mustache::load_text("index.html");
+
+    return page;
+  });
 
   app.multithreaded().port(18083).run();
   return 0;
