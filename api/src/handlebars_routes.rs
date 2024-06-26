70f97fb8febc91cf211d52a16dd7b1aa7bf92de7
ilotterytea <me@ilotterytea.kz>
2024-06-26 22:20:56 +0500

    feat: index template

--- api/src/handlebars_routes.rs
+++ api/src/handlebars_routes.rs
@@ -3,6 +3,7 @@ use actix_web_lab::respond::Html;
 use handlebars::Handlebars;
 use include_dir::{include_dir, Dir};
 use serde_json::json;
+use std::env::var;
 
 const HANDLEBARS_TEMPLATES: Dir = include_dir!("$CARGO_MANIFEST_DIR/templates");
 
@@ -18,7 +19,13 @@ pub fn load_handlebars_templates(hb: &mut Handlebars<'_>) {
 }
 
 pub async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
-    let data = json!({});
+    let contact_name: String = var("WEB_CONTACT_NAME").unwrap_or("someone".into());
+    let contact_url: String = var("WEB_CONTACT_URL").unwrap_or("#".into());
+
+    let data = json!({
+        "contact_name": contact_name,
+        "contact_url": contact_url
+    });
 
     let body = hb.render("index.html", &data).unwrap();
 
