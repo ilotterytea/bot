b2e34c29f7f9f3c8bc9b1eb33250ec2815e33b4e
ilotterytea <iltsu@alright.party>
2025-04-16 23:06:44 +0500

    upd: !chatters command

--- luamods/massping.lua
+++ luamods/massping.lua
@@ -1,46 +1,26 @@
 return {
     name = "massping",
+    description = "Ping em! :MegaLUL:",
     delay_sec = 5,
     options = {},
     subcommands = {},
     minimal_rights = "moderator",
     handle = function(request)
-        chatters = twitch_get_chatters()
+        local chatters = twitch_get_chatters()
 
-        m = ""
+        local m = ""
 
         if request.message ~= nil then
             m = request.message .. " ·"
         end
 
-        base = "📣 " .. m .. " "
-        userlines = { "" }
-        index = 1
+        local base = "📣 " .. m .. " "
 
-        max_line_length = 500
-
+        local names = {}
         for i = 1, #chatters, 1 do
-            chatter = chatters[i]
-            curmsg = userlines[index]
-            x = "@" .. chatter.login
-
-            if #base + #curmsg + 1 + #x >= max_line_length then
-                index = index + 1
-            end
-
-            if index > #userlines then
-                table.insert(userlines, x)
-            else
-                userlines[index] = curmsg .. " " .. x
-            end
+            table.insert(names, chatters[i].login)
         end
 
-        msgs = {}
-
-        for i = 1, #userlines, 1 do
-            table.insert(msgs, base .. userlines[i])
-        end
-
-        return msgs
+        return str_make_parts(base, names, "@", " ", 500)
     end
 }
