64e0eaf39dd17da3d17f398a3794a0c775c9af91
ilotterytea <iltsu@alright.party>
2025-12-04 16:54:17 +0500

    upd: delay for !lua and !luaimport

--- bot/src/commands/lua.hpp
+++ bot/src/commands/lua.hpp
@@ -19,52 +19,52 @@
 #include "schemas/user.hpp"
 #include "utils/string.hpp"
 
-void print_lua_object_type(const sol::object& obj);
+void print_lua_object_type(const sol::object &obj);
 
 namespace bot::command::lua {
   namespace library {
     void add_bot_library(std::shared_ptr<sol::state> state);
     void add_bot_library(std::shared_ptr<sol::state> state,
-                         const InstanceBundle& bundle);
+                         const InstanceBundle &bundle);
     void add_time_library(std::shared_ptr<sol::state> state);
     void add_json_library(std::shared_ptr<sol::state> state);
     void add_twitch_library(std::shared_ptr<sol::state> state,
-                            const Request& request,
-                            const InstanceBundle& bundle);
+                            const Request &request,
+                            const InstanceBundle &bundle);
     void add_kick_library(std::shared_ptr<sol::state> state,
-                          const InstanceBundle& bundle);
+                          const InstanceBundle &bundle);
     void add_net_library(std::shared_ptr<sol::state> state);
     void add_db_library(std::shared_ptr<sol::state> state,
-                        const Configuration& config);
+                        const Configuration &config);
     void add_irc_library(std::shared_ptr<sol::state> state,
-                         const InstanceBundle& bundle);
+                         const InstanceBundle &bundle);
     void add_l10n_library(std::shared_ptr<sol::state> state,
-                          const InstanceBundle& bundle);
+                          const InstanceBundle &bundle);
     void add_storage_library(std::shared_ptr<sol::state> state,
-                             const Request& request, const Configuration& cfg,
-                             const std::string& lua_id);
+                             const Request &request, const Configuration &cfg,
+                             const std::string &lua_id);
 
     void add_rss_library(std::shared_ptr<sol::state> state);
 
     void add_base_libraries(std::shared_ptr<sol::state> state);
     void add_chat_libraries(std::shared_ptr<sol::state> state,
-                            const Request& request,
-                            const InstanceBundle& bundle);
+                            const Request &request,
+                            const InstanceBundle &bundle);
   }
 
-  command::Response run_safe_lua_script(const Request& request,
-                                        const InstanceBundle& bundle,
-                                        const std::string& script,
+  command::Response run_safe_lua_script(const Request &request,
+                                        const InstanceBundle &bundle,
+                                        const std::string &script,
                                         std::string lua_id = "");
 
   class LuaCommand : public Command {
     public:
       LuaCommand(std::shared_ptr<sol::state> luaState,
-                 const std::string& content);
+                 const std::string &content);
       ~LuaCommand() = default;
 
-      Response run(const InstanceBundle& bundle,
-                   const Request& request) const override;
+      Response run(const InstanceBundle &bundle,
+                   const Request &request) const override;
 
       std::string get_name() const override { return this->name; }
       int get_delay_seconds() const override { return this->delay; }
@@ -93,10 +93,10 @@ namespace bot::command::lua {
     class LuaExecution : public command::Command {
         std::string get_name() const override { return "lua"; }
 
-        int get_delay_seconds() const override { return 1; }
+        int get_delay_seconds() const override { return 0; }
 
-        command::Response run(const InstanceBundle& bundle,
-                              const command::Request& request) const override {
+        command::Response run(const InstanceBundle &bundle,
+                              const command::Request &request) const override {
           if (!bundle.configuration.lua.allow_arbitrary_scripts &&
               request.requester.user_rights.get_level() < schemas::TRUSTED) {
             throw ResponseException<ResponseError::ILLEGAL_COMMAND>(
@@ -117,10 +117,10 @@ namespace bot::command::lua {
     class LuaRemoteExecution : public command::Command {
         std::string get_name() const override { return "luaimport"; }
 
-        int get_delay_seconds() const override { return 2; }
+        int get_delay_seconds() const override { return 0; }
 
-        command::Response run(const InstanceBundle& bundle,
-                              const command::Request& request) const override {
+        command::Response run(const InstanceBundle &bundle,
+                              const command::Request &request) const override {
           if (!request.message.has_value()) {
             throw ResponseException<ResponseError::NOT_ENOUGH_ARGUMENTS>(
                 request, bundle.localization, command::CommandArgument::VALUE);
@@ -130,7 +130,7 @@ namespace bot::command::lua {
               request.requester.user_rights.get_level() < schemas::TRUSTED &&
               !std::any_of(bundle.configuration.lua.script_whitelist.begin(),
                            bundle.configuration.lua.script_whitelist.end(),
-                           [&request](const std::string& i) {
+                           [&request](const std::string &i) {
                              return i == request.message.value();
                            })) {
             throw ResponseException<ResponseError::ILLEGAL_COMMAND>(
@@ -172,7 +172,7 @@ namespace bot::command::lua {
           std::string contentType = response.header["Content-Type"];
           if (!std::any_of(
                   mimeTypes.begin(), mimeTypes.end(),
-                  [&contentType](const auto& x) { return x == contentType; })) {
+                  [&contentType](const auto &x) { return x == contentType; })) {
             throw ResponseException<ResponseError::INCORRECT_ARGUMENT>(
                 request, bundle.localization, url);
           }
