fc36d0ecf94e63efee2cbfe822c6d0f1b1e64746
ilotterytea <iltsu@alright.party>
2025-12-24 01:31:30 +0500

    fix: temperature can be zero sometimes

--- luascripts/ping.lua
+++ luascripts/ping.lua
@@ -9,11 +9,15 @@ return {
     subcommands = {},
     minimal_rights = "user",
     handle = function(request)
+        local temperature = math.ceil(bot_get_temperature())
+        local temperature_str = ""
+        if temperature > 0 then
+            temperature_str = " · Temperature: " .. tostring(temperature) .. "°C"
+        end
         return request.sender.alias_name .. ": PotFriend Pong! " ..
             "Uptime: " .. time_humanize(bot_get_uptime()) ..
             " · Used memory: " .. math.ceil(bot_get_memory_usage() / 1024) ..
-            "MB · Temperature: " ..
-            tostring(math.ceil(bot_get_temperature())) .. "°C · Bot running on " .. bot_get_version() ..
+            "MB" .. temperature_str .. " · Bot running on " .. bot_get_version() ..
             " (Last updated " .. time_humanize(time_current() - bot_get_compile_time()) .. " ago)"
     end
 }
