9e716bdba2e7bf9123f65669d24988d1e45dc3ea
ilotterytea <iltsu@alright.party>
2026-04-14 16:51:44 +0500

    upd: dont pass helixclient with const

--- bot/src/utils/events.cpp
+++ bot/src/utils/events.cpp
@@ -5,10 +5,10 @@
 #include <vector>
 
 namespace bot::utils {
-  std::vector<schemas::Event> get_events(std::unique_ptr<db::BaseDatabase> conn,
-                                         api::twitch::HelixClient &api_client,
-                                         int moderator_id, int type,
-                                         const std::string &name) {
+  std::vector<schemas::Event> get_events(
+      std::unique_ptr<db::BaseDatabase> conn,
+      const api::twitch::HelixClient &api_client, int moderator_id, int type,
+      const std::string &name) {
     std::vector<schemas::Event> events;
 
     db::DatabaseRows rows = conn->exec(
--- bot/src/utils/events.hpp
+++ bot/src/utils/events.hpp
@@ -8,8 +8,8 @@
 #include "schemas/event.hpp"
 
 namespace bot::utils {
-  std::vector<schemas::Event> get_events(std::unique_ptr<db::BaseDatabase> conn,
-                                         api::twitch::HelixClient &api_client,
-                                         int moderator_id, int type,
-                                         const std::string &name);
+  std::vector<schemas::Event> get_events(
+      std::unique_ptr<db::BaseDatabase> conn,
+      const api::twitch::HelixClient &api_client, int moderator_id, int type,
+      const std::string &name);
 };
