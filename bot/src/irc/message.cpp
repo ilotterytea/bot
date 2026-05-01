05ecce8e9a655afbce7f8ad07c77f7af98b0559c
ilotterytea <iltsu@alright.party>
2026-05-01 18:33:30 +0300

    fix: segfault in irc parser

--- bot/src/irc/message.cpp
+++ bot/src/irc/message.cpp
@@ -32,40 +32,50 @@ namespace bot::irc {
   std::optional<IRCMessage> IRCMessage::from_string(std::string msg) {
     IRCMessage m;
 
+    if (msg.empty()) return std::nullopt;
+
     if (msg[0] == '@') {
-      int end = msg.find(' ');
-      std::string raw_tags = msg.substr(1, end);
-      for (const std::string tag : utils::string::split_text(raw_tags, ';')) {
-        auto splitted_tag = utils::string::split_text_n(tag, "=", 1);
-        m.tags.insert_or_assign(splitted_tag[0], splitted_tag[1]);
+      size_t end = msg.find(' ');
+      if (end == std::string::npos) return std::nullopt;
+
+      std::string raw_tags = msg.substr(1, end - 1);
+
+      for (const std::string &tag : utils::string::split_text(raw_tags, ';')) {
+        auto splitted = utils::string::split_text_n(tag, "=", 1);
+        if (splitted.size() == 2)
+          m.tags.insert_or_assign(splitted[0], splitted[1]);
       }
+
       msg = msg.substr(end + 1);
+      if (msg.empty()) return std::nullopt;
     }
 
     if (msg[0] == ':') {
-      int end = msg.find(' ');
-      m.prefix = msg.substr(1, end);
-      int bang = msg.find('!');
-      if (bang != std::string::npos) {
-        m.nick = m.prefix.substr(0, bang - 1);
-      }
+      size_t end = msg.find(' ');
+      if (end == std::string::npos) return std::nullopt;
+
+      m.prefix = msg.substr(1, end - 1);
+
+      size_t bang = m.prefix.find('!');
+      if (bang != std::string::npos) m.nick = m.prefix.substr(0, bang);
+
       msg = msg.substr(end + 1);
+      if (msg.empty()) return std::nullopt;
     }
 
-    std::vector<std::string> parts = utils::string::split_text(msg, ' ');
+    auto parts = utils::string::split_text(msg, ' ');
+    if (parts.empty()) return std::nullopt;
 
     m.command = parts.front();
     parts.erase(parts.begin());
 
-    for (int i = 0; i < parts.size(); i++) {
-      if (parts[i][0] == ':') {
-        parts = std::vector<std::string>(parts.begin() + i, parts.end());
-        m.params.push_back(utils::string::join_vector(parts, ' ').substr(1));
+    for (size_t i = 0; i < parts.size(); i++) {
+      if (!parts[i].empty() && parts[i][0] == ':') {
+        std::vector<std::string> trailing(parts.begin() + i, parts.end());
+        m.params.push_back(utils::string::join_vector(trailing, ' ').substr(1));
         break;
       }
-      if (!parts[i].empty()) {
-        m.params.push_back(parts[i]);
-      }
+      if (!parts[i].empty()) m.params.push_back(parts[i]);
     }
 
     return m;
